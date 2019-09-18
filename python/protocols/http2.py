325e25311b757cf5ca21a0373bfd8bcf36782a73
Blake Anderson <blaander@cisco.com>
2019-09-18 08:22:51 -0400

    pmercury: adding tls decrypt + http/* request/response data extraction

---
+++ python/protocols/http2.py
@@ -0,0 +1,246 @@
+from collections import OrderedDict
+from binascii import hexlify, unhexlify
+
+# http/2.0 imports
+from hpack import Decoder
+
+# constants
+from utils.http2_constants import *
+
+class HTTP2:
+    def __init__(self):
+        self.h2_decoder = {}
+        self.data_cache = {}
+
+    def fingerprint(self, data, flow_key, mode):
+        flow_key += mode
+        offset = 0
+        data_offset = None
+
+        if flow_key not in self.h2_decoder:
+            self.h2_decoder[flow_key] = Decoder()
+            self.h2_decoder[flow_key].max_allowed_table_size = 65536
+
+
+        if flow_key in self.data_cache and self.data_cache[flow_key][2]:
+            if self.data_cache[flow_key][0] + len(data) >= self.data_cache[flow_key][1]:
+                data_offset = self.data_cache[flow_key][1] - self.data_cache[flow_key][0]
+                self.data_cache[flow_key] = [0,0,False]
+            else:
+                self.data_cache[flow_key][0] += len(data)
+                data_offset = None
+                
+        offset_ = self.check_magic(data)
+        offset += offset_
+
+        http2 = self.parse_iterate(data, offset, flow_key)
+        if data_offset and http2 != None:
+            http2 = self.parse_iterate(data, data_offset, flow_key)
+
+        return http2
+
+    def get_human_readable(self, fp_str_):
+        t_ = [str(unhexlify(x[1:]),'utf-8') for x in fp_str_.split(b')')[:-1]]
+        fp_h = []
+        for i in range(len(t_)):
+            field = t_[i].split(': ',1)
+            fp_h.append({field[0]: field[1]})
+        return fp_h
+
+    def parse_iterate(self, data, offset, flow_key):
+        headers = None
+        # go through http/2 messages
+        while offset < len(data):
+            msg = OrderedDict({})
+            tmp_offset = offset
+
+            # robustness check
+            if len(data[offset:]) < 9:
+                break
+
+            # parse frame headers
+            tmp_type = int(hexlify(data[offset+3:offset+4]),16)
+            msg['length'] = int(hexlify(data[offset:offset+3]),16)
+            msg['stream_identifier'] = int(hexlify(data[offset+5:offset+9]),16)
+
+            # robustness against retransmissions, etc.
+            if tmp_type not in HTTP2_MESSAGE_TYPES or \
+               msg['stream_identifier'] > 1000 or \
+               msg['length'] > 1000000:
+                    break
+
+            # success
+            msg['type'] = HTTP2_MESSAGE_TYPES[tmp_type]
+            self.parse_flags(data[offset+4], msg, msg['type'])
+            offset += 9
+
+            if msg['length'] > len(data[offset:]):
+                self.data_cache[flow_key] = [len(data[offset:]),msg['length'],True]
+
+            # Parse frame types
+            if msg['type'] == 'DATA':
+                self.parse_data(msg, data, offset)
+            elif msg['type'] == 'HEADERS':
+                headers = self.parse_headers(msg, data, offset, flow_key)
+            elif msg['type'] == 'PRIORITY':
+                self.parse_priority(msg, data, offset)
+            elif msg['type'] == 'RST_STREAM':
+                self.parse_rst_stream(msg, data, offset)
+            elif msg['type'] == 'SETTINGS':
+                self.parse_settings(msg, data, offset)
+            elif msg['type'] == 'PING':
+                self.parse_ping(msg, data, offset)
+            elif msg['type'] == 'GOAWAY':
+                self.parse_goaway(msg, data, offset)
+            elif msg['type'] == 'WINDOW_UPDATE':
+                self.parse_window_update(msg, data, offset)
+
+            offset += msg['length']
+
+        return headers
+
+
+    def parse_headers(self, msg, data, offset, flow_key):
+        # parse data associated with flags
+        tmp_offset = 0
+        if 'flags' in msg:
+            if 'Padded' in msg['flags']:
+                msg['pad_length'] = data[offset]
+                offset += 1
+                tmp_offset -= 1
+            if 'Priority' in msg['flags']:
+                tmp_dependency = int(hexlify(data[offset:offset+4]),16)
+                msg['exclusive_stream_dependency'] = tmp_dependency & int('80000000',16)
+                msg['depends_on_stream'] = tmp_dependency & int('7FFFFFFF',16)
+                offset += 4
+                msg['weight'] = int(hexlify(data[offset:offset+1]),16) + 1
+                offset += 1
+                tmp_offset -= 5
+
+        # get just the data associated with the headers
+        if 'pad_length' in msg:
+            header_data = data[offset:msg['length']+offset+tmp_offset-msg['pad_length']]
+        else:
+            header_data = data[offset:msg['length']+offset+tmp_offset]
+
+        # use hpack to parse headers
+        try:
+            headers = self.h2_decoder[flow_key].decode(header_data)
+            msg['headers'] = []
+            fp_str = b''
+            for (header, value) in headers:
+                fp_str += b'(' + hexlify(bytes(header,'utf-8')) +\
+                          hexlify(b': ') + hexlify(bytes(value,'utf-8')) + b')'
+                msg['headers'].append({header: value})
+            return fp_str
+        except:
+            msg['status'] = 'failed to parse headers'
+            msg['data'] = hexlify(header_data)
+
+
+    def parse_data(self, msg, data, offset):
+        # parse data associated with flags
+        if 'flags' in msg and 'Padded' in msg['flags']:
+            msg['pad_length'] = data[offset]
+            offset += 1
+
+        # parse body and store it as hex
+        msg['body'] = hexlify(data[offset:])
+
+    def parse_priority(self, msg, data, offset):
+        if len(data[offset:]) < msg['length']:
+            msg['priority'] = 'failure: no data'
+            return
+
+        # parse exclusive
+        msg['exclusive'] = int(hexlify(data[offset:offset+4]),16) & int('80000000',16)
+
+        # parse last stream id
+        msg['stream_dependency'] = (int(hexlify(data[offset:offset+4]),16) << 1) >> 1
+
+        # parse weight
+        msg['weight'] = data[offset+4]
+
+    def parse_rst_stream(self, msg, data, offset):
+        # parse error code
+        error_code = int(hexlify(data[offset:offset+4]),16)
+        if error_code in HTTP2_ERROR_CODES:
+            error_code = HTTP2_ERROR_CODES[error_code]
+        else:
+            error_code = 'unknown_error_code_%i' % error_code
+        msg['error_code'] = error_code
+
+    def parse_ping(self, msg, data, offset):
+        # parse 8-bytes of PING data
+        #  This data can be anything, but typically is all zeros
+        msg['data'] = hexlify(data[offset:offset+8])
+
+    def parse_settings(self, msg, data, offset):
+        if len(data[offset:]) < msg['length']:
+            msg['settings'] = 'failure: no data'
+            return
+
+        msg['settings'] = OrderedDict({})
+        # settings format: 2 byte identifier, 4 byte value
+        for i in range(0,msg['length'],6):
+            s_type = int(hexlify(data[offset+i:offset+i+2]),16)
+            if s_type in HTTP2_SETTINGS_TYPES:
+                s_type = HTTP2_SETTINGS_TYPES[s_type]
+            else:
+                s_type = 'unknown_setting_%i' % s_type
+            s_value = int(hexlify(data[offset+i+2:offset+i+6]),16)
+            msg['settings'][s_type] = s_value
+
+    def parse_goaway(self, msg, data, offset):
+        # parse last stream id
+        msg['last_stream_id'] = (int(hexlify(data[offset:offset+4]),16) << 1) >> 1
+
+        # parse error code
+        error_code = int(hexlify(data[offset+4:offset+8]),16)
+        if error_code in HTTP2_ERROR_CODES:
+            error_code = HTTP2_ERROR_CODES[error_code]
+        else:
+            error_code = 'unknown_error_code_%i' % error_code
+        msg['error_code'] = error_code
+
+        debug_data = hexlify(data[offset+8:offset+msg['length']])
+        if len(debug_data) > 0:
+            msg['debug_data'] = debug_data
+
+    def parse_window_update(self, msg, data, offset):
+        # parse the 31-bit window size increment
+        if len(data[offset:]) < 4:
+            msg['window_size_increment'] = 'failure: no data'
+            return
+
+        msg['window_size_increment'] = (int(hexlify(data[offset:offset+4]),16) << 1) >> 1
+
+    def parse_flags(self, data, msg, frame_type):
+        data = data
+        if data == 0:
+            return
+
+        msg['flags'] = OrderedDict({})
+
+        if frame_type == 'SETTINGS' or frame_type == 'PING':
+            if data & 1:
+                msg['flags']['Ack'] = True
+        else:
+            if data & 1:
+                msg['flags']['End_Stream'] = True
+
+        if data & 4:
+            msg['flags']['End_Headers'] = True
+        if data & 8:
+            msg['flags']['Padded'] = True
+        if data & 32:
+            msg['flags']['Priority'] = True
+
+    def check_magic(self, data):
+        offset = 0
+
+        # pre-defined string in the spec, seems to be a joke on PRISM
+        if hexlify(data).startswith(b'505249202a20485454502f322e300d0a0d0a534d0d0a0d0a'):
+            offset = 24
+
+        return offset
---
+++ python/utils/tls_crypto.py
@@ -0,0 +1,385 @@
+import struct
+from binascii import hexlify, unhexlify
+
+# crypto primitive imports
+from cryptography.hazmat.primitives.hmac import HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers.modes import GCM, CBC
+from cryptography.hazmat.primitives.hashes import SHA1, SHA256, SHA384, MD5, Hash
+from cryptography.hazmat.primitives.ciphers.algorithms import AES, ARC4, TripleDES, Camellia, SEED
+
+# constants
+from utils.tls_constants import *
+
+
+class TLS_CRYPTO:
+
+    def __init__(self):
+        self.cur_mode = None
+        self.session_metadata = None
+        self.tls_sequence = None
+        self.tls13_handshake = None
+        self.kdf = {}
+        self.kdf['TLS 1.0'] = self.kdf_tls10
+        self.kdf['TLS 1.1'] = self.kdf_tls11
+        self.kdf['TLS 1.2'] = self.kdf_tls12
+        self.kdf['TLS 1.3'] = self.kdf_tls13
+
+
+    def kdf_tls10(self, cr, sr, secret, cipher_params, flow_key):
+        if flow_key+self.cur_mode not in self.session_metadata:
+            self.session_metadata[flow_key+self.cur_mode] = {}
+        if 'cbc_initial_decrypt' not in self.session_metadata[flow_key+self.cur_mode]:
+            self.session_metadata[flow_key+self.cur_mode]['cbc_initial_decrypt'] = 1
+            fixed_iv_length = cipher_params['iv_length']
+        else:
+            fixed_iv_length = cipher_params['fixed_iv_length']
+        label = b'key expansion'
+
+        secret_md5 = secret[:len(secret)/2]
+        secret_sha = secret[-len(secret)/2:]
+
+        md5_material = b''
+        cur_hash = self.hmac(secret_md5, MD5(), b'%s%s%s' % (label, sr, cr))
+        for i in range(16):
+            md5_material += self.hmac(secret_md5, MD5(), b'%s%s%s%s' % (cur_hash, label, sr, cr))
+            cur_hash = self.hmac(secret_md5, MD5(), cur_hash)
+        
+        sha_material = b''
+        cur_hash = self.hmac(secret_sha, SHA1(), b'%s%s%s' % (label, sr, cr))
+        for i in range(16):
+            sha_material += self.hmac(secret_sha, SHA1(), b'%s%s%s%s' % (cur_hash, label, sr, cr))
+            cur_hash = self.hmac(secret_sha, SHA1(), cur_hash)
+
+        output = b''
+        for i in range(min(len(md5_material),len(sha_material))):
+            output += chr(ord(md5_material[i]) ^ ord(sha_material[i]))
+
+        key_material_lengths = [cipher_params['mac_key_length']]*2 + \
+                               [cipher_params['enc_key_length']]*2 + \
+                               [fixed_iv_length]*2
+
+        offset = 0
+        key_material = []
+        for l in key_material_lengths:
+            key_material.append(output[offset:offset+l])
+            offset += l
+
+        return key_material
+
+
+    def kdf_tls11(self, cr, sr, secret, cipher_params, flow_key):
+        label = b'key expansion'
+
+        secret_md5 = secret[:len(secret)/2]
+        secret_sha = secret[-len(secret)/2:]
+
+        md5_material = b''
+        cur_hash = self.hmac(secret_md5, MD5(), b'%s%s%s' % (label, sr, cr))
+        for i in range(16):
+            md5_material += self.hmac(secret_md5, MD5(), b'%s%s%s%s' % (cur_hash, label, sr, cr))
+            cur_hash = self.hmac(secret_md5, MD5(), cur_hash)
+        
+        sha_material = b''
+        cur_hash = self.hmac(secret_sha, SHA1(), b'%s%s%s' % (label, sr, cr))
+        for i in range(16):
+            sha_material += self.hmac(secret_sha, SHA1(), b'%s%s%s%s' % (cur_hash, label, sr, cr))
+            cur_hash = self.hmac(secret_sha, SHA1(), cur_hash)
+
+        output = b''
+        for i in range(min(len(md5_material),len(sha_material))):
+            output += chr(ord(md5_material[i]) ^ ord(sha_material[i]))
+
+        key_material_lengths = [cipher_params['mac_key_length']]*2 + \
+                               [cipher_params['enc_key_length']]*2 + \
+                               [cipher_params['fixed_iv_length']]*2
+
+        offset = 0
+        key_material = []
+        for l in key_material_lengths:
+            key_material.append(output[offset:offset+l])
+            offset += l
+
+        return key_material
+
+
+    def kdf_tls12(self, cr, sr, secret, cipher_params, flow_key):
+        label = b'key expansion'
+        digest_type = cipher_params['prf']()
+
+        cur_hash = self.hmac(secret, digest_type, b'%s%s%s' % (label, sr, cr))
+        output = ''
+        for i in range(16):
+            output += self.hmac(secret, digest_type, b'%s%s%s%s' % (cur_hash, label, sr, cr))
+            cur_hash = self.hmac(secret, digest_type, cur_hash)
+
+        key_material_lengths = [cipher_params['mac_key_length']]*2 + \
+                               [cipher_params['enc_key_length']]*2 + \
+                               [cipher_params['fixed_iv_length']]*2
+        offset = 0
+        key_material = []
+        for l in key_material_lengths:
+            key_material.append(output[offset:offset+l])
+            offset += l
+
+        return key_material
+
+
+    def kdf_tls13(self, secret, label, length, cipher_params, flow_key):
+        digest_type = cipher_params['prf']()
+        key = b''
+        block = b''
+
+        ind = 0
+        while len(key) < length:
+            ind += 1
+            block = self.hmac(secret, digest_type, b'%s%s%s' % (block, label, struct.pack('B',ind)))
+            key += block
+
+        return key[:length]
+
+
+    def hmac(self, secret, digest_type, msg):
+        tmp = HMAC(secret, digest_type, default_backend())
+        tmp.update(msg)
+        return tmp.finalize()
+
+
+    def hash_(self, digest_type, msg):
+        tmp = Hash(digest_type, default_backend())
+        tmp.update(msg)
+        return tmp.finalize()
+
+
+    def get_secret(self, client_random, secrets, cur_flow_key):
+        secret = None
+        if client_random not in secrets:
+            return None
+
+        if not self.session_metadata['version'].startswith('TLS 1.3'):
+            secret = unhexlify(secrets[client_random]['master_secret'])
+
+        # find appropriate master secret
+        if cur_flow_key not in self.tls13_handshake:
+            self.tls13_handshake[cur_flow_key] = True
+        if self.cur_mode == 'client' and self.tls13_handshake[cur_flow_key] == True and \
+           'client_handshake_secret' in secrets[client_random]:
+            secret = unhexlify(secrets[client_random]['client_handshake_secret'])
+        elif self.cur_mode == 'server' and self.tls13_handshake[cur_flow_key] == True and \
+           'server_handshake_secret' in secrets[client_random]:
+            secret = unhexlify(secrets[client_random]['server_handshake_secret'])
+        elif self.cur_mode == 'client' and self.tls13_handshake[cur_flow_key] == False and \
+             'client_traffic_secret' in secrets[client_random]:
+            secret = unhexlify(secrets[client_random]['client_traffic_secret'])
+        elif self.cur_mode == 'server' and self.tls13_handshake[cur_flow_key] == False and \
+             'server_traffic_secret' in secrets[client_random]:
+            secret = unhexlify(secrets[client_random]['server_traffic_secret'])
+
+        return secret
+
+
+    def get_explicit_material(self, flow_key, data, cipher_params):
+        enc = None
+        iv = None
+
+        if self.session_metadata['version'] == 'TLS 1.0':
+            enc = data
+            if cipher_params['mode'] == CBC:
+                if flow_key+self.cur_mode not in self.session_metadata or \
+                   'cbc_initial_decrypt' not in self.session_metadata[flow_key+self.cur_mode] or \
+                   'cur_iv' not in self.session_metadata[flow_key+self.cur_mode]:
+                    iv = ''
+                else:
+                    iv = self.session_metadata[flow_key+self.cur_mode]['cur_iv']
+        elif self.session_metadata['version'] in ['TLS 1.1','TLS 1.2']:
+            enc = data[cipher_params['iv_length']:]
+            iv = data[:cipher_params['iv_length']]
+        elif self.session_metadata['version'].startswith('TLS 1.3'):
+            enc = data
+            iv = b''
+
+        return enc, iv
+
+
+    def get_implicit_material(self, client_random, server_random, master_secret, \
+                              cipher_params, flow_key, explicit_iv):
+        key = None
+        iv = None
+
+        if self.session_metadata['version'] in ['SSL 3.0','TLS 1.0','TLS 1.1','TLS 1.2']:
+            c_mac_key, s_mac_key, c_key, s_key, c_iv, s_iv = \
+                self.kdf[self.session_metadata['version']](client_random, server_random, \
+                                                           master_secret, cipher_params, flow_key)
+            if self.cur_mode == 'client':
+                key = c_key
+                iv = c_iv + explicit_iv
+            else:
+                key = s_key
+                iv = s_iv + explicit_iv
+        elif self.session_metadata['version'].startswith('TLS 1.3'):
+            cur_flow_key = flow_key + self.cur_mode
+            label_str = ''
+            if self.session_metadata['version'] == 'TLS 1.3' or self.session_metadata['version'] == 'TLS 1.3 (draft 20)':
+                label_str = b'tls13 '
+            else:
+                label_str = b'TLS 1.3, '
+            tmp_label = label_str + b'key'
+            len_ = struct.pack(b'!H', cipher_params['enc_key_length'])
+            tmp_label = b'%s%s%s%s' % (len_, struct.pack(b'B', len(tmp_label)), tmp_label, b'\x00')
+            key = self.kdf_tls13(master_secret, tmp_label, cipher_params['enc_key_length'], \
+                                 cipher_params, flow_key)
+
+            tmp_label = label_str + b'iv'
+            len_ = struct.pack(b'!H', cipher_params['iv_length'])
+            tmp_label = b'%s%s%s%s' % (len_, struct.pack(b'B', len(tmp_label)), tmp_label, b'\x00')
+            implicit_iv = self.kdf_tls13(master_secret, tmp_label, cipher_params['iv_length'], \
+                                         cipher_params, flow_key)
+            
+            # calculate nonce
+            iv2 = struct.pack(b'!Q', self.tls_sequence[cur_flow_key]).rjust(len(implicit_iv), b'\x00')
+            iv = b''.join([struct.pack(b'B', v ^ implicit_iv[i]) for i, v in enumerate(iv2)])
+
+        return key, iv
+
+
+    # strip MAC/AEAD/Padding
+    def get_data(self, result, flow_key, cipher_params, encrypted_data):
+        padding_length = 0
+
+        # strip padding
+        if self.session_metadata['version'].startswith('TLS 1.3'):
+            for i in range(len(result)-1,-1,-1):
+                if result[i] != b'\x00':
+                    break
+                padding_length += 1
+            result = result[:-padding_length-1]
+        else:
+            if cipher_params['mode'] == CBC:
+                padding_length = int(hexlify(result[-1:]),16)
+                if len(result) < padding_length+1:
+                    padding_length = 0
+                else:
+                    for i in range(1,padding_length+1):
+                        if int(hexlify(result[-(i+1):-i]),16) != padding_length:
+                            padding_length = 0
+                            break
+                if padding_length != 0:
+                    padding_length += 1
+                    result = result[:-padding_length]
+
+                # set up IV for TLS 1.0
+                if self.session_metadata['version'] == 'TLS 1.0':
+                    if flow_key+self.cur_mode not in self.session_metadata:
+                        self.session_metadata[flow_key+self.cur_mode] = {}
+                    self.session_metadata[flow_key+self.cur_mode]['cur_iv'] = encrypted_data[-cipher_params['iv_length']:]
+
+        # strip AEAD/MAC
+        auth_length = 0
+        if cipher_params['mode'] == GCM:
+            if cipher_params['enc_key_length'] == 32:
+                result = result[:-16]
+            elif cipher_params['enc_key_length'] == 16:
+                result = result
+            auth_length = cipher_params['enc_key_length']
+        elif cipher_params['mac_key_length'] > 0:
+            result = result[:-cipher_params['mac_key_length']]
+            auth_length = cipher_params['mac_key_length']
+
+        return result, padding_length, auth_length
+
+
+    # get encrypted data and crypto parameters, output plaintext
+    def get_plaintext(self, data, cipher_params, key, iv, flow_key):
+        if cipher_params['cipher'] == AES:
+            if cipher_params['mode'] == CBC:
+                decryptor = Cipher(cipher_params['cipher'](key), \
+                                   cipher_params['mode'](iv), \
+                                   default_backend()).decryptor()
+            if cipher_params['mode'] == GCM:
+                if len(data[-16:]) < 16:
+                    return None
+                decryptor = Cipher(cipher_params['cipher'](key), \
+                                   cipher_params['mode'](iv,data[-16:]), \
+                                   default_backend()).decryptor()
+        elif cipher_params['cipher'] == ARC4:
+            if flow_key+self.cur_mode not in self.session_metadata:
+                self.session_metadata[flow_key+self.cur_mode] = {}
+            if 'decryptor' not in self.session_metadata[flow_key+self.cur_mode]:
+                self.session_metadata[flow_key+self.cur_mode]['decryptor'] = \
+                    decryptor = Cipher(cipher_params['cipher'](key), \
+                                       None,
+                                       default_backend()).decryptor()
+            decryptor = self.session_metadata[flow_key+self.cur_mode]['decryptor']
+        elif cipher_params['cipher'] == TripleDES:
+                decryptor = Cipher(cipher_params['cipher'](key), \
+                                   cipher_params['mode'](iv), \
+                                   default_backend()).decryptor()
+        elif cipher_params['cipher'] == Camellia:
+                decryptor = Cipher(cipher_params['cipher'](key), \
+                                   cipher_params['mode'](iv), \
+                                   default_backend()).decryptor()
+        elif cipher_params['cipher'] == SEED:
+                decryptor = Cipher(cipher_params['cipher'](key), \
+                                   cipher_params['mode'](iv), \
+                                   default_backend()).decryptor()
+        else:
+            print('%s Not Supported' % cipher_params['cipher'])
+            return None
+
+        return decryptor.update(data)
+
+
+    # Main decrypt function
+    def decrypt(self, data, flow_key, cur_mode, session_metadata, tls_sequence, secrets, tls13_handshake):
+        self.cur_mode = cur_mode
+        self.session_metadata = session_metadata
+        self.tls_sequence = tls_sequence
+        self.tls13_handshake = tls13_handshake
+
+        if 'selected_cipher_suite' not in self.session_metadata or 'client_random' not in self.session_metadata \
+           or 'server_random' not in self.session_metadata:
+            return None, None, None
+        cur_flow_key = flow_key + self.cur_mode
+        if self.session_metadata['selected_cipher_suite'] not in TLS_CIPHER_SUITES:
+            print('NYI:\t' + self.session_metadata['selected_cipher_suite'])
+            return None, None, None
+
+        cipher_params = TLS_CIPHER_SUITES[self.session_metadata['selected_cipher_suite']]
+        client_random = self.session_metadata['client_random']
+        server_random = self.session_metadata['server_random']
+
+        # set initial sequence number for decryption
+        if cur_flow_key not in self.tls_sequence:
+            self.tls_sequence[cur_flow_key] = 0
+
+        # get master secret, varies for TLS 1.3
+        master_secret = self.get_secret(client_random, secrets, cur_flow_key)
+        if master_secret == None:
+            return None, None, None
+
+        # get encrypted data and (if necessary) explicit iv
+        encrypted_data, explicit_iv = \
+            self.get_explicit_material(flow_key, data, cipher_params)
+        if encrypted_data == None or explicit_iv == None:
+            return None, None, None
+
+        # get encryption key and implicit iv
+        key, iv = self.get_implicit_material(unhexlify(client_random), \
+                        unhexlify(server_random), master_secret, cipher_params, \
+                        flow_key, explicit_iv)
+
+        # decrypt encrypted text
+        result = self.get_plaintext(encrypted_data, cipher_params, key, iv, flow_key)
+        if result == None:
+            return None, None, None
+
+        # determine if padding is used
+        result, padding_length, auth_length = self.get_data(result, flow_key, \
+                                               cipher_params, encrypted_data)
+
+        # update sequence number
+        self.tls_sequence[cur_flow_key] += 1
+
+        return result, padding_length, auth_length
+
