cea0cc564b741a4a3eb7e59fc18eabe3e2c80f4c
Blake Anderson <blaander@cisco.com>
2019-09-22 17:10:32 -0400

    pmercury: more prep for pip package

---
+++ python/setup.py
@@ -0,0 +1,53 @@
+"""     
+ Copyright (c) 2019 Cisco Systems, Inc. All rights reserved.
+ License at https://github.com/cisco/mercury/blob/master/LICENSE
+"""
+
+import setuptools
+import site
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='pmercury',
+    version='0.1.0.000',
+    description='Python tool for network (TLS, etc.) fingerprinting',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: System :: Networking :: Monitoring',
+        'Topic :: Security',
+    ],
+    keywords='tls fingerprinting network traffic analysis',
+    url='https://github.com/cisco/mercury/',
+    author='Blake Anderson',
+    author_email='blake.anderson@cisco.com',
+    license_files=['../LICENSE'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'cryptography',
+        'dpkt',
+        'hpack',
+        'numpy',
+        'pyasn',
+        'pypcap',
+        'ujson',
+    ],
+    scripts=['pmercury/pmercury'],
+    data_files=[('/pmercury/resources', ['../resources/fingerprint_db.json.gz',
+                                         '../resources/app_families.txt',
+                                         '../resources/asn_info.db.gz',
+                                         '../resources/implementation_date_cs.json.gz',
+                                         '../resources/public_suffix_list.dat.gz',
+                                         '../resources/implementation_date_ext.json.gz',
+                                         '../resources/pyasn.db.gz']
+    )],
+    include_package_data=True,
+    zip_safe=False
+)
