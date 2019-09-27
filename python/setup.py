4da891ad9dd8c51a7314eb1f4151a31e5ca17b62
Blake Anderson <blaander@cisco.com>
2019-09-27 13:17:28 -0400

    pmercury: minor version bump

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setuptools.setup(
     name='pmercury',
-    version='0.1.0.000',
+    version='0.1.1.000',
     description='Python tool for network (TLS, etc.) fingerprinting',
     long_description=readme(),
     long_description_content_type="text/markdown",
