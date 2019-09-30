9711d05037f511f44641dd9035fe0e4e2ea6b7c1
David McGrew <mcgrew@cisco.com>
2019-09-30 16:32:09 -0400

    configure checks for python3 packages, but does not install them

---
+++ package_test.py
@@ -0,0 +1,10 @@
+# run as: python3 package_test.py
+#
+# return value will be 0 (success) only if all needed packages are available
+
+import cython
+import ujson
+import pyasn
+
+print ("found python packages cython, ujson, pyasn")
+
