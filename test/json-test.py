3f32d9a531e4d4f8488deaf5efd32246fc87131b
Blake Anderson <blaander@cisco.com>
2019-10-06 17:57:02 -0400

    updating schema testing

--- test/json-test.py
+++ test/json-test.py
@@ -1,38 +1,68 @@
 #!/bin/python
 #
-# USAGE: json-test.py <jsonfilename>
+# USAGE: json-test-v2.py <jsonfilename>
 #
 # checks JSON file format for correctness against the mercury JSON schema
 #
 # RETURN: 0 on success, nonzero otherwise
 
 
+import jsonschema
 import json
 import sys
 
-def test_mercury_json_line(line):
-    x = json.loads(line)
+mercury_schema = {
+    'type':       'object',
+    'properties': {
+        'src_ip':       {'type': 'string'},
+        'dst_ip':       {'type': 'string'},
+        'src_port':     {'type': 'number'},
+        'dst_port':     {'type': 'number'},
+        'protocol':     {'type': 'number'},
+        'event_start':  {'type': 'number'},
+        'fingerprints': {'type': 'object',
+                         'properties': {
+                             'tls': {'type': 'string'},
+                             'tcp': {'type': 'string'},
+                         },
+        },
+        'tls': {'type': 'object',
+                'properties': {
+                    'server_name': {'type': 'string'},
+                }
+        }
+    },
+    'required': ['src_ip','dst_ip','src_port','dst_port','protocol','event_start','fingerprints']
+}
 
-    sa = x['src_ip']
-    da = x['dst_ip']
-    sp = x['src_port']
-    dp = x['dst_port']
-    pr = x['protocol']
-    time = x['event_start']
-    fingerprints = x['fingerprints']
-    if 'tls' in fingerprints and '(0000)' in fingerprints['tls']:
-        tls = x['tls']
-        sni = tls['server_name']
+
+def test_mercury_json_line(line):
+    try:
+        jsonschema.validate(instance=json.loads(line), schema=mercury_schema)
+    except Exception as e:
+        print(e)
+        return -1
+
+    return 0
+
     
 def main():
     if len(sys.argv) != 2:
         print('usage: %s <jsonfilename>' % sys.argv[0])
         return -1 # error
     inputfilename = sys.argv[1]
-    
+
+    total  = 0
+    failed = 0
     for line in open(inputfilename):
-        test_mercury_json_line(line)
-    
+        ret = test_mercury_json_line(line)
+        total += 1
+        if ret == -1:
+            failed += 1
+
+    print('%% Failed:\t%0.2f%%' % (100.*failed/float(total)))
+
   
 if __name__== "__main__":
   main()
+
