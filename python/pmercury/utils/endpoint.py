8a7c83787072df4bde46e439a6df532b659cd56d
Blake Anderson <blaander@tip-tools-03.cisco.com>
2019-11-04 17:15:18 +0000

    pmercury: endpoint modeling updates

--- python/pmercury/utils/endpoint.py
+++ python/pmercury/utils/endpoint.py
@@ -9,10 +9,53 @@ class Endpoint:
 
 
     def update(self, flow):
-        src = flow['src_ip']
-        fp_type = list(flow['fingerprints'].keys())[0]
-        fp_ = flow['fingerprints'][fp_type]
+        fp_type = next(iter(flow['fingerprints']))
+        fp_     = flow['fingerprints'][fp_type]
+        src     = self.get_src(flow, fp_type)
 
+        if src not in self.endpoints:
+            self.endpoints[src] = {}
+            self.endpoints[src]['summary'] = {}
+            self.endpoints[src]['prev_flow'] = None
+        if fp_type not in self.endpoints[src]:
+            self.endpoints[src]['summary'][fp_type] = {}
+
+        self.endpoints[src]['prev_flow'] = flow
+
+        if fp_ not in self.endpoints[src]['summary'][fp_type]:
+            self.endpoints[src]['summary'][fp_type][fp_] = {}
+            self.endpoints[src]['summary'][fp_type][fp_]['count'] = 0
+
+        self.endpoints[src]['summary'][fp_type][fp_]['count'] += 1
+        if fp_type in flow:
+            if 'context' not in self.endpoints[src]['summary'][fp_type][fp_]:
+                self.endpoints[src]['summary'][fp_type][fp_]['context'] = {}
+            for k_ in flow[fp_type]:
+                if k_ not in self.endpoints[src]['summary'][fp_type][fp_]['context']:
+                    self.endpoints[src]['summary'][fp_type][fp_]['context'][k_] = {}
+                if flow[fp_type][k_] not in self.endpoints[src]['summary'][fp_type][fp_]['context'][k_]:
+                    self.endpoints[src]['summary'][fp_type][fp_]['context'][k_][flow[fp_type][k_]] = 0
+                self.endpoints[src]['summary'][fp_type][fp_]['context'][k_][flow[fp_type][k_]] += 1
+
+
+    def write_all(self, out):
+        for id_ in self.endpoints:
+            o_ = {}
+            o_['identifier'] = id_
+            o_['fingerprints'] = self.endpoints[id_]
+
+            out.write(json.dumps(o_) + '\n')
+
+
+    def get_prev_flow(self, src):
+        try:
+            return self.endpoints[src]['prev_flow']
+        except KeyError:
+            return None
+
+
+    def get_src(self, flow, fp_type):
+        src = flow['src_ip']
         if fp_type == 'dhcp':
             if fp_type in flow and 'client_mac_address' in flow[fp_type] and 'requested_ip' in flow[fp_type]:
                 mac = flow[fp_type]['client_mac_address']
@@ -25,36 +68,7 @@ class Endpoint:
                 self.endpoints[src_mac] = deepcopy(self.endpoints[src])
                 del self.endpoints[src]
             src = src_mac
-            if src not in self.endpoints:
-                self.endpoints[src] = {}
-        elif src not in self.endpoints:
-            self.endpoints[src] = {}
 
-        if fp_type not in self.endpoints[src]:
-            self.endpoints[src][fp_type] = {}
-
-        if fp_ not in self.endpoints[src][fp_type]:
-            self.endpoints[src][fp_type][fp_] = {}
-            self.endpoints[src][fp_type][fp_]['count'] = 0
-
-        self.endpoints[src][fp_type][fp_]['count'] += 1
-        if fp_type in flow:
-            if 'context' not in self.endpoints[src][fp_type][fp_]:
-                self.endpoints[src][fp_type][fp_]['context'] = {}
-            for k_ in flow[fp_type]:
-                if k_ not in self.endpoints[src][fp_type][fp_]['context']:
-                    self.endpoints[src][fp_type][fp_]['context'][k_] = {}
-                if flow[fp_type][k_] not in self.endpoints[src][fp_type][fp_]['context'][k_]:
-                    self.endpoints[src][fp_type][fp_]['context'][k_][flow[fp_type][k_]] = 0
-                self.endpoints[src][fp_type][fp_]['context'][k_][flow[fp_type][k_]] += 1
-
-
-    def write_all(self, out):
-        for id_ in self.endpoints:
-            o_ = {}
-            o_['identifier'] = id_
-            o_['fingerprints'] = self.endpoints[id_]
-
-            out.write(json.dumps(o_) + '\n')
+        return src
 
 
