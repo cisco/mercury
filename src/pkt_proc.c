ead5d7b02892b6434a5e1c0589c87a3a91852571
David McGrew <mcgrew@cisco.com>
2019-12-11 04:29:06 -0800

    cleaned up error handling and c/c++ boundary

--- src/pkt_proc.c
+++ src/pkt_proc.c
@@ -23,66 +23,68 @@ unsigned int packet_filter_threshold = 8;
 struct pkt_proc *pkt_proc_new_from_config(struct mercury_config *cfg,
                                           int tnum,
                                           char *fileset_id) {
-    enum status status;
-    char outfile[MAX_FILENAME];
-    pid_t pid = tnum; // syscall(__NR_gettid);
 
-    uint64_t max_records = 0;
-    if (cfg->rotate) {
-        max_records = cfg->rotate;
-    }
+    try {
 
-    if (cfg->write_filename) {
+        enum status status;
+        char outfile[MAX_FILENAME];
+        pid_t pid = tnum; 
 
-        status = filename_append(outfile, cfg->write_filename, "/", fileset_id);
-        if (status) {
-            throw "error in filename";
-        }
-        if (cfg->verbosity) {
-            printf("initializing thread function %x with filename %s\n", pid, outfile);
+        uint64_t max_records = 0;
+        if (cfg->rotate) {
+            max_records = cfg->rotate;
         }
 
-        if (cfg->filter) {
-            /*
-             * write only TLS clientHellos and TCP SYNs to capture file
-             */
-            return new pkt_proc_filter_pcap_writer(outfile, cfg->flags);
+        if (cfg->write_filename) {
+
+            status = filename_append(outfile, cfg->write_filename, "/", fileset_id);
             if (status) {
-                printf("error: could not open pcap output file %s\n", outfile);
-                throw "error in pcap output file";
+                throw "error in filename";
             }
+            if (cfg->verbosity) {
+                printf("initializing thread function %x with filename %s\n", pid, outfile);
+            }
+
+            if (cfg->filter) {
+                /*
+                 * write only packet metadata (TLS clientHellos, TCP SYNs, ...) to capture file
+                 */
+                return new pkt_proc_filter_pcap_writer(outfile, cfg->flags);
+
+            } else {
+                /*
+                 * write all packets to capture file
+                 */
+                return new pkt_proc_pcap_writer(outfile, cfg->flags);
+
+            }
+
+        } else if (cfg->fingerprint_filename) {
+            /*
+             * write fingerprints into output file
+             */
+            status = filename_append(outfile, cfg->fingerprint_filename, "/", fileset_id);
+            if (status) {
+                throw "error in filename";
+            }
+            if (cfg->verbosity) {
+                printf("initializing thread function %x with filename %s\n", pid, outfile);
+            }
+
+            return new pkt_proc_json_writer(outfile, cfg->mode, max_records);
+
         } else {
             /*
-             * write all packets to capture file
+             * default: dump JSON-formatted packet info to stdout
              */
-            return new pkt_proc_pcap_writer(outfile, cfg->flags);
+            return new pkt_proc_dumper();
 
         }
-
-    } else if (cfg->fingerprint_filename) {
-        /*
-         * write fingerprints into output file
-         */
-        status = filename_append(outfile, cfg->fingerprint_filename, "/", fileset_id);
-        if (status) {
-            throw "error in filename";
-        }
-        if (cfg->verbosity) {
-            printf("initializing thread function %x with filename %s\n", pid, outfile);
-        }
-
-        return new pkt_proc_json_writer(outfile, cfg->mode, max_records);
-        if (status) {
-            perror("error: could not open fingerprint output file");
-            throw "error opening json file";
-        }
-    } else {
-        /*
-         * default: dump JSON-formatted packet info to stdout
-         */
-        return new pkt_proc_dumper();
 
     }
+    catch (const char *s) {
+        fprintf(stdout, "error: %s\n", s);
+    };
 
     return NULL;
 }
