5be09e763879f24c595ad05432fbc1f52505b347
David McGrew <mcgrew@cisco.com>
2019-12-23 04:44:24 -0800

    cleaned up json_file_write()

--- src/json_file_io.c
+++ src/json_file_io.c
@@ -89,135 +89,127 @@ void fprintf_timestamp(FILE *f, unsigned int sec, unsigned int usec) {
 }
 
 void json_file_write(struct json_file *jf,
-		     uint8_t *packet,
-		     size_t length,
-		     unsigned int sec,
-		     unsigned int usec) {
+                     uint8_t *packet,
+                     size_t length,
+                     unsigned int sec,
+                     unsigned int usec) {
     extern unsigned int packet_filter_threshold;
     FILE *file = jf->file;
     struct packet_filter pf;
     pf.tcp_init_msg_filter = NULL;
- 
+
+    /*
+     * apply packet filter to packet; return if no fingerprints or metadata found
+     */
     size_t bytes_extracted = packet_filter_extract(&pf, packet, length);
+    if (bytes_extracted <= packet_filter_threshold && pf.x.packet_data.type == packet_data_type_none) {
+        return;
+    }
+
+    /*
+     * output fingerprint (if any)
+     */
     if (bytes_extracted > packet_filter_threshold) {
         uint8_t *extractor_buffer = pf.x.output_start;
-	switch(pf.x.fingerprint_type) {
-    case fingerprint_type_dhcp_client:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"dhcp\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"},");
-        break;
-	case fingerprint_type_tls:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"tls\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"}");
-	    if (pf.x.packet_data.type == packet_data_type_tls_sni) {
-		if (pf.x.packet_data.length >= SNI_HDR_LEN) {
-		    fprintf(file, ",\"tls\":{");
-		    fprintf_json_string(file,
-					"server_name",
-					pf.x.packet_data.value  + SNI_HDR_LEN,
-					pf.x.packet_data.length - SNI_HDR_LEN);
-		    fprintf(file, "}");
-		}
-	    }
-	    fprintf(file, ",");
-
-	    break;
-	case fingerprint_type_tcp:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"tcp\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"},");
-	    break;
-	case fingerprint_type_http:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"http\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"},");
-	    fprintf(file, "\"complete\":\"%s\",", (pf.x.proto_state.state == state_done) ? "yes" : "no");
-
-	    if (pf.x.packet_data.type == packet_data_type_http_user_agent) {
-		fprintf(file, "\"http\":{");
-		fprintf_json_string(file,
-					"user_agent",
-					pf.x.packet_data.value,
-					pf.x.packet_data.length);
-		fprintf(file, "},");
-	    }
-	    break;
-	case fingerprint_type_http_server:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"http_server\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"},");
-	    fprintf(file, "\"complete\":\"%s\",", (pf.x.proto_state.state == state_done) ? "yes" : "no");
-	    break;
-	case fingerprint_type_tls_server:
-	  fprintf(file, "{");
-	  /* check if we have tls_server fingerprint */
-	  if (extractor_get_output_length(&pf.x) > 0) {
-            fprintf(file, "\"fingerprints\":{");
-            fprintf(file, "\"tls_server\":\"");
+        switch(pf.x.fingerprint_type) {
+        case fingerprint_type_dhcp_client:
+            fprintf(file, "{\"fingerprints\":{\"dhcp\":\"");
             fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
             fprintf(file, "\"},");
-	  }
-	  
-	  break;
-	case fingerprint_type_dtls:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"dtls\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"}");
-	    if (pf.x.packet_data.type == packet_data_type_tls_sni) {
-		if (pf.x.packet_data.length >= SNI_HDR_LEN) {
-		    fprintf(file, ",\"dtls\":{");
-		    fprintf_json_string(file,
-					"server_name",
-					pf.x.packet_data.value  + SNI_HDR_LEN,
-					pf.x.packet_data.length - SNI_HDR_LEN);
-		    fprintf(file, "}");
-		}
-	    }
-	    fprintf(file, ",");
+            break;
+        case fingerprint_type_tls:
+            fprintf(file, "{\"fingerprints\":{\"tls\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            break;
+        case fingerprint_type_tcp:
+            fprintf(file, "{\"fingerprints\":{\"tcp\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            break;
+        case fingerprint_type_http:
+            fprintf(file, "{\"fingerprints\":{\"http\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            fprintf(file, "\"complete\":\"%s\",", (pf.x.proto_state.state == state_done) ? "yes" : "no");
+            break;
+        case fingerprint_type_http_server:
+            fprintf(file, "{\"fingerprints\":{\"http_server\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            fprintf(file, "\"complete\":\"%s\",", (pf.x.proto_state.state == state_done) ? "yes" : "no");
+            break;
+        case fingerprint_type_tls_server:
+            fprintf(file, "{\"fingerprints\":{\"tls_server\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            break;
+        case fingerprint_type_dtls:
+            fprintf(file, "{\"fingerprints\":{\"dtls\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            break;
+        case fingerprint_type_dtls_server:
+            fprintf(file, "{\"fingerprints\":{dtls_server\":\"");
+            fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
+            fprintf(file, "\"},");
+            break;
+        default:
+            ;    /* no fingerprint; do nothing */
+        }
 
-	    break;
-	case fingerprint_type_dtls_server:
-	    fprintf(file, "{\"fingerprints\":{");
-	    fprintf(file, "\"dtls_server\":\"");
-	    fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    fprintf(file, "\"},");
-	    break;
-	default:
-	    /* print nothing */
-	    return;
-	    //fprintf(file, "\"unknown\":\"");
-	    //fprintf_binary_ept_as_paren_ept(file, extractor_buffer, bytes_extracted);
-	    //fprintf(file, "\"},");
-	}
+        /*
+         * output packet_data (if any)
+         */
+        if (pf.x.packet_data.type == packet_data_type_http_user_agent) {
+            fprintf(file, "\"http\":{");
+            fprintf_json_string(file,
+                                "user_agent",
+                                pf.x.packet_data.value,
+                                pf.x.packet_data.length);
+            fprintf(file, "},");
+        }
+        if (pf.x.packet_data.type == packet_data_type_tls_sni) {
+            if (pf.x.packet_data.length >= SNI_HDR_LEN) {
+                fprintf(file, "\"tls\":{");
+                fprintf_json_string(file,
+                                    "server_name",
+                                    pf.x.packet_data.value  + SNI_HDR_LEN,
+                                    pf.x.packet_data.length - SNI_HDR_LEN);
+                fprintf(file, "},");
+            }
+        }
+        if (pf.x.packet_data.type == packet_data_type_tls_cert) {
+            /* print the certificates in base64 format */
+            fprintf(file, "\"tls\":{\"server_certs\":[");
+            extract_certificates(file, pf.x.packet_data.value, pf.x.packet_data.length);
+            fprintf(file, "]},");
+        }
+        if (pf.x.packet_data.type == packet_data_type_dtls_sni) {
+            if (pf.x.packet_data.length >= SNI_HDR_LEN) {
+                fprintf(file, "\"dtls\":{");
+                fprintf_json_string(file,
+                                    "server_name",
+                                    pf.x.packet_data.value  + SNI_HDR_LEN,
+                                    pf.x.packet_data.length - SNI_HDR_LEN);
+                fprintf(file, "},");
+            }
+        }
 
-	/* check if we have any certificates */
-	if (pf.x.packet_data.type == packet_data_type_tls_cert) {
-	  /* print the certificates in base64 format */
-	  fprintf(file, "\"tls\":{");
-	  fprintf(file, "\"server_certs\":[");
-	  extract_certificates(file, pf.x.packet_data.value, pf.x.packet_data.length);
-	  fprintf(file, "]},");
-	}
+        /*
+         * output flow key, analysis (if it's configured), and the timestamp
+         */
+        struct flow_key key = flow_key_init();
+        flow_key_set_from_packet(&key, packet, length);
+        fprintf_analysis_from_extractor_and_flow_key(file, &pf.x, &key);
 
-	struct flow_key key = flow_key_init();
-	flow_key_set_from_packet(&key, packet, length);
-	fprintf_analysis_from_extractor_and_flow_key(file, &pf.x, &key);
+        packet_fprintf_flow_key(file, packet, length);
+        fprintf_timestamp(file, sec, usec);
+        fprintf(file, "}\n");
 
-	packet_fprintf_flow_key(file, packet, length);
-	fprintf_timestamp(file, sec, usec);
-	fprintf(file, "}\n");
-
-	if (json_file_needs_rotation(jf)) {
-	    json_file_rotate(jf);
-	}
+        if (json_file_needs_rotation(jf)) {
+            json_file_rotate(jf);
+        }
     }
 
 }
