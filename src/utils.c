028ca3e0abf05fbf552856b95736020bf4c69b60
David McGrew <mcgrew@cisco.com>
2020-01-03 06:32:00 -0800

    removed dead code (unused AF_PACKET TPACKET version 1 IO)

--- src/utils.c
+++ src/utils.c
@@ -401,3 +401,46 @@ void printf_raw_as_hex(const uint8_t *data, unsigned int len) {
         printf("\n");
     }
 }
+
+#define MAX_READABLE_SUFFIX 9
+char *readable_number_suffix[MAX_READABLE_SUFFIX] = {
+    (char *)"",
+    (char *)"K",
+    (char *)"M",
+    (char *)"G",
+    (char *)"T",
+    (char *)"P",
+    (char *)"E",
+    (char *)"Z",
+    (char *)"Y"
+};
+
+void get_readable_number_int(unsigned int power,
+                             unsigned int input,
+                             unsigned int *num_output,
+                             char **str_output) {
+    unsigned int index = 0;
+
+    while ((input > power) && ((index + 1) < MAX_READABLE_SUFFIX)) {
+	index++;
+	input = input / power;
+    }
+    *num_output = input;
+    *str_output = readable_number_suffix[index];
+
+}
+
+void get_readable_number_float(double power,
+                               double input,
+                               double *num_output,
+                               char **str_output) {
+    unsigned int index = 0;
+
+    while ((input > power) && ((index + 1) < MAX_READABLE_SUFFIX)) {
+	index++;
+	input = input / power;
+    }
+    *num_output = input;
+    *str_output = readable_number_suffix[index];
+
+}
