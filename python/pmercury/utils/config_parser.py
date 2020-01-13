8446ee72d1b0fb43acc625030cb4f8c7940078ac
Blake Anderson <blaander@cisco.com>
2020-01-13 13:52:53 -0500

    pmercury: updating configuration to use yaml

--- python/pmercury/utils/config_parser.py
+++ python/pmercury/utils/config_parser.py
@@ -1,10 +1,11 @@
-"""     
+"""
  Copyright (c) 2019 Cisco Systems, Inc. All rights reserved.
  License at https://github.com/cisco/mercury/blob/master/LICENSE
 """
 
 import os
 import sys
+import yaml
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../')
@@ -19,27 +20,7 @@ def parse_config(f_):
     if not os.path.exists(config_file):
         return None
 
-    config = {}
-    cur_proto = None
-    cur_type  = None
-    for line in open(config_file):
-        line = line.strip()
-        if line == '' or line.startswith('#'):
-            continue
-
-        if line in protocols:
-            cur_proto = line
-            continue
-
-        if cur_proto not in config:
-            config[cur_proto] = {}
-
-        try:
-            cur_type, params = line.split()
-            config[cur_proto][cur_type] = params.split(',')
-        except:
-            print('error: unknown configuration file format (cur_proto)')
-            return None
+    with open(config_file, 'r') as yaml_cfg:
+        config = yaml.safe_load(yaml_cfg)
 
     return config
-
