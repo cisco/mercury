01b89307cd1f24af818dc7d5685f5303969405a0
Blake Anderson <blaander@cisco.com>
2020-08-24 10:18:00 -0400

    tls fingerprint database update

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setuptools.setup(
     name='pmercury',
-    version='0.5.2.45',
+    version='0.5.2.46',
     description='Python tool for network (TLS, etc.) fingerprinting',
     long_description=readme(),
     long_description_content_type="text/markdown",
