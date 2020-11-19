2dff38586005f40b1d14a35f4dd0047a8f0089b0
David McGrew <mcgrew@cisco.com>
2020-11-19 12:12:40 -0500

    statefull_pkt_proc reorganization

--- src/pkt_proc.h
+++ src/pkt_proc.h
@@ -36,6 +36,63 @@ struct pkt_proc_stats {
     size_t packets_written;
 };
 
+
+struct stateful_pkt_proc {
+    struct packet_filter pf;
+    struct flow_table ip_flow_table;
+    struct flow_table_tcp tcp_flow_table;
+    struct tcp_reassembler reassembler;
+    struct tcp_reassembler *reassembler_ptr;
+
+    explicit stateful_pkt_proc(const char *filter) :
+        pf{},
+        ip_flow_table{65536},
+        tcp_flow_table{65536},
+        reassembler{65536},
+        reassembler_ptr{&reassembler}
+    {
+        if (packet_filter_init(&pf, filter) == status_err) {
+            throw "could not initialize packet filter";
+        }
+#ifndef USE_TCP_REASSEMBLY
+// #pragma message "omitting tcp reassembly; 'make clean' and recompile with OPTFLAGS=-DUSE_TCP_REASSEMBLY to use that option"
+        reassembler_ptr = nullptr;
+#else
+// #pragma message "using tcp reassembly; 'make clean' and recompile to omit that option"
+#endif
+
+    }
+
+    void finalize() {
+        reassembler.count_all();
+        tcp_flow_table.count_all();
+    }
+
+    size_t write_json(void *buffer,
+                      size_t buffer_size,
+                      uint8_t *packet,
+                      size_t length,
+                      struct timespec *ts) {
+        return write_json(buffer, buffer_size, packet, length, ts, reassembler_ptr);
+    }
+
+    size_t write_json(void *buffer,
+                              size_t buffer_size,
+                              uint8_t *packet,
+                              size_t length,
+                              struct timespec *ts,
+                              struct tcp_reassembler *reassembler);
+
+    void tcp_data_write_json(struct buffer_stream &buf,
+                             struct datum &pkt,
+                             const struct key &k,
+                             struct tcp_packet &tcp_pkt,
+                             struct timespec *ts,
+                             struct tcp_reassembler *reassembler);
+
+};
+
+
 /*
  * struct pkt_proc is a packet processor; this abstract class defines
  * the interface to packet processing that can be used by packet
@@ -175,15 +232,65 @@ struct pkt_proc_filter_pcap_writer : public pkt_proc {
 };
 
 /*
+ * struct pkt_proc_json_writer_llq represents a packet processing object
+ * that writes out a JSON representation of fingerprints, metadata,
+ * flow keys, and event time to a queue that is then written to a file
+ * by a dedicated output thread.
+ */
+struct pkt_proc_json_writer_llq : public pkt_proc {
+    struct ll_queue *llq;
+    bool block;
+    struct stateful_pkt_proc processor;
+
+    /*
+     * pkt_proc_json_writer(outfile_name, mode, max_records)
+     * initializes object to write a single JSON line containing the
+     * flow key, time, fingerprints, and metadata to the output file
+     * with the path outfile_name and mode passed as arguments; that
+     * file is opened by this invocation, with that mode.  If
+     * max_records is nonzero, then it defines the maximum number of
+     * records (lines) per file; after that limit is reached, file
+     * rotation will take place.
+     */
+    explicit pkt_proc_json_writer_llq(struct ll_queue *llq_ptr, const char *filter, bool blocking) :
+        block{blocking},
+        processor{filter}
+    {
+        llq = llq_ptr;
+    }
+
+    void apply(struct packet_info *pi, uint8_t *eth) override {
+        struct llq_msg *msg = llq->init_msg(block, pi->ts.tv_sec, pi->ts.tv_nsec);
+        if (msg) {
+            size_t write_len = processor.write_json(msg->buf, LLQ_MSG_SIZE, eth, pi->len, &(msg->ts));
+            if (write_len > 0) {
+                msg->send(write_len);
+                llq->increment_widx();
+            }
+        }
+    }
+
+    void finalize() override {
+        processor.finalize();
+    }
+
+    void flush() override {
+
+    }
+
+};
+
+
+/*
  * struct pkt_proc_filter_pcap_writer represents a packet processing
- * object that first filters packets, then writes tem out in PCAP file
+ * object that first filters packets, then writes them out in PCAP file
  * format.
  */
 struct pkt_proc_filter_pcap_writer_llq : public pkt_proc {
     struct ll_queue *llq;
-    struct packet_filter pf;
     bool block;
-    
+    struct stateful_pkt_proc processor;
+
     /*
      * packet_filter_threshold is a (somewhat arbitrary) threshold used in
      * the packet metadata filter; it will probably get eliminated soon,
@@ -191,11 +298,8 @@ struct pkt_proc_filter_pcap_writer_llq : public pkt_proc {
      */
     unsigned int packet_filter_threshold = 8;
 
-    explicit pkt_proc_filter_pcap_writer_llq(struct ll_queue *llq_ptr, const char *filter, bool blocking) : block{blocking} {
+    explicit pkt_proc_filter_pcap_writer_llq(struct ll_queue *llq_ptr, const char *filter, bool blocking) : block{blocking}, processor{filter} {
         llq = llq_ptr;
-        if (packet_filter_init(&pf, filter) == status_err) {
-            throw "could not initialize packet filter";
-        }
     }
 
     void apply(struct packet_info *pi, uint8_t *eth) override {
@@ -208,7 +312,8 @@ struct pkt_proc_filter_pcap_writer_llq : public pkt_proc {
             return;  /* random packet drop configured, and this packet got selected to be discarded */
         }
 
-        if (packet_filter_apply(&pf, packet, length)) {
+        uint8_t buf[LLQ_MSG_SIZE];
+        if (processor.write_json(buf, LLQ_MSG_SIZE, packet, length, &pi->ts) != 0) {
             pcap_queue_write(llq, eth, pi->len, pi->ts.tv_sec, pi->ts.tv_nsec / 1000, block);
         }
     }
