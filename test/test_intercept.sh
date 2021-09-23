f75b6cda281ffce4b1f110b070a9f0e52e15dd0c
David McGrew <mcgrew@cisco.com>
2021-09-23 17:18:04 -0400

    adding test script for intercept.so

---
+++ test/test_intercept.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# test_intercept.sh
+#
+# tests the intercept.so library
+
+echo "testing intercept.so library for plaintext interception"
+
+# set up shell variables, and change working directory
+#
+intercept_dir=`pwd`/tmpdir
+LD_PRELOAD=`pwd`/../src/intercept.so
+mkdir $intercept_dir
+echo "using output directory $intercept_dir"
+cd $intercept_dir
+
+sites=(accounts.google.com amazon.com apple.com bbc.com bp.blogspot.com cloudflare.com cnn.com creativecommons.org developers.google.com docs.google.com drive.google.com dropbox.com en.wikipedia.org es.wikipedia.org europa.eu facebook.com fr.wikipedia.org github.com google.de googleusercontent.com gstatic.com issuu.com istockphoto.com line.me linkedin.com mail.google.com maps.google.com mozilla.org myspace.com netvibes.com paypal.com play.google.com plus.google.com Root Domain sites.google.com support.google.com t.me uol.com.br vimeo.com vk.com whatsapp.com who.int wordpress.org www.blogger.com www.google.com www.yahoo.com youtu.be youtube.com)
+
+# start interception by exporting variables
+#
+export intercept_dir
+export LD_PRELOAD
+
+# loop over sites
+#
+echo "looping over all web servers"
+blank="                                                       "
+for s in ${sites[@]}; do
+    echo -ne "visiting $s $blank \r\c" >> /dev/stderr
+    wget https://$s >> wget-output.txt 2>&1
+    curl --verbose https://$s > $s.curl >> curl-output.txt 2>&1
+done
+echo "done $blank"
+
+# verify that output JSON file is valid
+cat intercept.json | jq . > /dev/null
+
+echo "intercept JSON output is valid"
+
+cd ..
+
+# note: we could retain the tmpdir/ directory, but for now we remove it
+rm -rf tmpdir/
+
+# EOF
