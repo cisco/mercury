0fa8fe840e4075f2388c7caf75f8e7cc426c90b3
David McGrew <mcgrew@cisco.com>
2021-09-28 14:46:19 -0400

    adding fprint_hex

--- src/libmerc/datum.h
+++ src/libmerc/datum.h
@@ -335,6 +335,14 @@ struct datum {
         }
         return std::numeric_limits<int>::min();
     }
+
+    void fprint_hex(FILE *f) {
+        const uint8_t *x = data;
+        while (x < data_end) {
+            fprintf(f, "%02x", *x++);
+        }
+    }
+
 };
 
 template <size_t T> struct data_buffer {
