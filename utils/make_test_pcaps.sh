0f72512ad0aadfa0f6809fda610759b38e385f06
David McGrew <mcgrew@cisco.com>
2021-10-07 04:00:52 -0700

    adding cases, streamlining code

--- utils/make_test_pcaps.sh
+++ utils/make_test_pcaps.sh
@@ -14,12 +14,31 @@ fi
 
 input=$1
 output=`basename $1`
-tshark -r $input -Y "dns.flags.response==0" -F pcap -w dns.query.$output
-tshark -r $input -Y "dns.flags.response==1" -F pcap -w dns.response.$output
-tshark -r $input -Y "http.request"  -F pcap -w http.request.$output
-tshark -r $input -Y "http.response"  -F pcap -w http.response.$output
-tshark -r $input -Y "tls.handshake.type==1" -F pcap -w tls.client_hello.$output
-tshark -r $input -Y "tls.handshake.type==2" -F pcap -w tls.server_hello.$output
-tshark -r $input -Y "quic" -F pcap -w quic.$output
 
+# silce_file <filter> <output file prefix>
+#
+# takes as input a tshark/wireshark display filter and an associated
+# output file prefix, and runs the filter against the input PCAP file,
+# to create another PCAP file that contains only packets that match
+# the filter, which is named based on the output file prefix
+#
+function slice_file {
+    filter=$1
+    outfile="$2.$output"
+    if [ ! -f $outfile ]; then
+        tshark -r $input -Y $filter -F pcap -w $outfile
+    else
+        echo "file $outfile exists, skipping"
+    fi
+}
+
+slice_file "dns.flags.response==1" "dns.query"
+slice_file "dns.flags.response==1" "dns.response"
+slice_file "http.request"          "http.request"
+slice_file "http.response"         "http.response"
+slice_file "tls.handshake.type==1" "tls.client_hello"
+slice_file "tls.handshake.type==2" "tls.server_hello"
+slice_file "dtls.handshake.type==1" "dtls.client_hello"
+slice_file "dtls.handshake.type==2" "dtls.server_hello"
+slice_file "quic" "quic"                              # TODO: add filter detail
 
