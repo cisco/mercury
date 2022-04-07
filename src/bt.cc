58603b82b6417ffa415815597c27f4c5ecfc13a1
David McGrew <mcgrew@cisco.com>
2022-04-07 12:04:19 -0400

    updates

--- src/bt.cc
+++ src/bt.cc
@@ -2,12 +2,52 @@
 
 #include <array>
 #include "libmerc/bittorrent.h"
+#include "hex.h"
 
-template <size_t N>
-datum get_datum(const std::array<uint8_t, N> &a) {
-    datum tmp{a.data(), a.data() + a.size()};
-    return tmp;
-}
+
+/*
+
+   Creating a packet parser using class datum
+
+   1.  Define a class with a single constructor that accepts a 'datum &' argument.
+
+   2.  The class should only have private data members, not public ones.
+
+   3.  The data members should represent the data features of interest
+       in the packet, in the order that the appear in the packet.
+
+   4.  Data members should be initialied in the same order.
+
+   Creating a new protocol that is connected into packet recognition
+   and processing (pkt_proc/proto_ident)
+
+   1.  Create a new class for each message in the protocol that you
+       want to recognize and process.  It should be a subclass of
+       tcp_base_protocol.  (TODO: rename that class to base_protocol).
+
+   2.  Define an implementation of is_not_empty() and write_json()
+       appropriate for your protocol.
+
+   3.  If the protocol can be fingerprinted, implement a compute_fingerprint() function.
+
+   4.  If the protocol fingerprint can be analyzed, implement a do_analysis() function.
+
+   5.  Create a static constexpr mask_and_value<N> that can be used to
+       identify the new protocol from its initial N bytes.  It may be necessary
+       to create more than one matcher.  (TODO: describe how to determine a mask and value
+       using the strings program, and/or create a new program to do that.)
+
+   6. For each new protocol class defined in step 1, add an entry in
+      the appropriate place in the traffic_selector class, that will
+      install the matchers in the packet identification function.
+      Note that the order matters, especially for protocols like QUIC
+      that have ambiguous formats.
+
+ */
+
+ //
+// start of new http_request implementation
+//
 
 // class acceptor accepts and ignores a single character, setting d to
 // null if the expected character is not found
@@ -40,19 +80,6 @@ public:
         this->data_end = d.data;
     }
 };
-
-template <size_t N>
-struct literal_string {
-    literal_string(const std::array<uint8_t, N> & ) {
-        // for (const auto &c : characters) {
-        //     d.accept(c);
-        // }
-    }
-};
-
-constexpr std::array<uint8_t, 3> GET{'G', 'E', 'T'};
-
-literal_string<3> tmp({'G', 'E', 'T'});
 
 // class lit is a literal string of characters
 //
@@ -143,21 +170,12 @@ public:
     }
 };
 
-//struct tuple<T, Ts...> : tuple<Ts...> {
-//  tuple(T t, Ts... ts) : tuple<Ts...>(ts...), tail(t) {}
 //
-//  T tail;
-//};
+// end of http_request implementation
+//
 
-template <class... Ts> struct literal {};
-
-template <class T, class... Ts>
-class literal<T, Ts...> : literal<Ts...> {
-    T tail;
-public:
-    literal(T t, Ts... ts) : literal<Ts...>(ts...), tail(t) {}
-};
-
+// cow_spam is an example of a benconding dictionary
+//
 class cow_spam {
     acceptor<'d'> d_acceptor;
     bencoding::key_and_value<bencoding::byte_string> cow;
@@ -174,15 +192,10 @@ public:
     }
 };
 
-#include "hex.h"
-
-//auto x = 0X1102030405060708abcdef_hexarr;
 
 auto http_req2 = 0x474554202f20485454502f312e310d0a436f6e6e656374696f6e3a206b6565702d616c6976650d0a436f6e74656e742d547970653a20746578742f68746d6c0d0a486f73743a20736572766963652e627269676874636c6f75642e636f6d0d0a436f6e74656e742d4c656e6774683a203535310d0a582d494d466f7277617264733a2032300d0a5669613a20312e312072747031302d646d7a2d7773612d322e636973636f2e636f6d3a38302028436973636f2d5753412f58290d0a582d466f727761726465642d466f723a2031302e38332e35302e3234370d0a0d0a3c3f427269676874436c6f75642076657273696f6e3d626361702f312e313f3e3c626361703e3c7365716e756d3e32373c2f7365716e756d3e3c656e63727970742d747970653e656e63727970742d76313c2f656e63727970742d747970653e364441454338313936414341354441343631334643364237344535444535453035363231424132303643353145313838384139353535433444323930413239443431344644463941304237413144343633453534364636374637344531433231454430323833393644333443314232463734333144414532364235433035373835373138363843333443373638413641453345443445323441363834444445394630384141424246464331433735454136433432423232463243303644303438343041434332384546434631374135353945433237323038323635383838313431433446394336393046444238333135343243354335303241433634413939434635314536464245393438314538373632353130303431393237343531394241363346394138363439374245383538304541383037323830413043383738333635443544313033304430304135433537303646364330434430433841434444304537343344334633313343343330324242313334394341383042343530343642413730383834323333383632453736414436353532463541333538433732393833333837393737384231354134433444343830433630393034373945423438333c2f626361703e_hex;
 
 int main() {
-
-    literal<char, char, char> abc('G', 'E', 'T');
 
     const unsigned char p1[] = "i357e";
     datum d1{p1, p1+strlen((char *)p1)};
@@ -190,12 +203,12 @@ int main() {
     printf("'%s' has value %ld\n", (char *)p1, b1.value());
 
     std::array<uint8_t, 4> p2{ 'i', '8', '9', 'e' };
-    datum d2 = get_datum(p2);
+    datum d2{p2};
     bencoding::bint b2(d2);
     printf("'%.*s' has value %ld\n", (int)p2.size(), (char *)p2.data(), b2.value());
 
     std::array<uint8_t, 6> p3{ '4', ':', 's', 'p', 'a', 'm' };
-    datum d3 = get_datum(p3);
+    datum d3{p3};
     bencoding::byte_string b3(d3);
     printf("'%.*s' has value ", (int)p3.size(), (char *)p3.data());
     b3.value().fprint(stdout);
