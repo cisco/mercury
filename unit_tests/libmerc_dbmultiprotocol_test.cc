7d174e05b547aceca98e924a8e8dc9eb7ddf3cca
vignvisw <vignvisw@cisco.com>
2022-07-26 02:52:54 -0400

    Adding smb filter unit test

--- unit_tests/libmerc_dbmultiprotocol_test.cc
+++ unit_tests/libmerc_dbmultiprotocol_test.cc
@@ -298,3 +298,35 @@ TEST_CASE_METHOD(LibmercTestFixture, "test dns and mdns with recources-mp")
         dns_check(count, config.m_lc);
     }
 }
+
+TEST_CASE_METHOD(LibmercTestFixture, "test smb with recources-mp")
+{
+
+    auto smb_check = [&](int expected_count, const struct libmerc_config &config)
+    {
+        initialize(config);
+
+        CHECK(expected_count == counter());
+
+        deinitialize();
+    };
+
+    std::vector<std::pair<test_config, int>> test_set_up{
+        {test_config{
+             .m_lc{.do_analysis = true, .resources = resources_mp_path,
+                .packet_filter_cfg = (char *)"smb"},
+             .m_pc{"smb.pcap"}},
+         391},
+        {test_config{
+             .m_lc{.do_analysis = true, .resources = resources_mp_path,
+                .packet_filter_cfg = (char *)"smb"},
+             .m_pc{"top_100_fingerprints.pcap"}},
+         0}
+    };
+
+    for (auto &[config, count] : test_set_up)
+    {
+        set_pcap(config.m_pc.c_str());
+        smb_check(count, config.m_lc);
+    }
+}
