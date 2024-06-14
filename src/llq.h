d6ec6a921fe9473163f99727d53f910f069f8a82
Brandon Enright <brenrigh@brandonenright.net>
2024-06-14 21:01:41 +0000

    Detailed ringbuffer comments

--- src/llq.h
+++ src/llq.h
@@ -25,20 +25,62 @@ struct llq_msg {
 /* a lockless ringbuffer */
 struct ll_queue {
     int qnum;           /* This is the queue number and is only needed for debugging */
+    uint8_t *rbuf;      /* The ringbuffer */
     uint64_t llq_len;   /* The length of the ringbuffer */
     uint64_t ridx;      /* The read index */
     uint64_t widx;      /* The write index */
-    int need_read;      /* Has writer wraped around and ran into reader */
+    int need_read;      /* Special case: writer wraped around and ran into reader */
     uint64_t drops;     /* Output drop counter */
-    uint8_t *rbuf;      /* The ringbuffer */
 
-    struct llq_msg * init_msg(bool blocking, unsigned int sec, unsigned int nsec) {
+
+    /* This lockless ringbuffer supports a thread writing separately
+     * from a thread reading without the need for locks. This is achieved with
+     * atomic loads and stores.
+     *
+     * Both writing and reading are two-step operations with two
+     * access member functions.
+     *
+     * writing: init_msg() followed by send()
+     * reading: try_read() followed by complete_read()
+     *
+     * init_msg() makes sure there is enough space free
+     * for a message (the struct header + the message bytes)
+     * so that writing doesn't run into the reader.
+     *
+     * send() recieves the true message size written (which is almost
+     * always less than the maximum allowed size) and updates the
+     * write index accordingly. send() handles wrapping the write
+     * index around as not to run into the end of the buffer.  There
+     * is an edge-case when sending where the message just written
+     * causes the write index to catch up to the read index (meaning
+     * the write index is actually so far ahead of the read index that
+     * it has wrapped around and run into reading). In this situation
+     * the writer must wait for the reader. This is different than the
+     * starting situation where the write and read index are equal
+     * because the reader is fully caught up and waiting on the
+     * writer.
+     *
+     * try_read() checks that either it is behind the writer or is in
+     * the special case described in send() where reading is so far
+     * behind the writer has caught up to the reader and must wait on
+     * the reader.
+     *
+     * complete_read() simply advances the read index and handles the
+     * buffer wrapping logic. Note that complete_read() does not need
+     * to handle clearing the special case flag because init_msg()
+     * checks and identifies when the reader has advanced again and
+     * the writer is no longer in the special case of needing to wait
+     * for the reader.
+     *
+     */
+
+    struct llq_msg * init_msg(bool blocking, unsigned int sec,
+    unsigned int nsec) {
 
         /* If we're blocking we gotta restart from here */
     blocking_retry_loop:
 
         struct llq_msg *m = (struct llq_msg *)&rbuf[widx];
-
 
         uint64_t cur_ridx = __atomic_load_n(&ridx, __ATOMIC_RELAXED);
         uint64_t cur_need_read = __atomic_load_n(&need_read, __ATOMIC_RELAXED);
@@ -55,8 +97,22 @@ struct ll_queue {
 
         if (cur_need_read == 0) {
             if (widx >= cur_ridx) {
+                /* In this case the space_available will always be
+                 * greater than is needed for another message because
+                 * the send() function always wraps the write idex
+                 * around anytime the write index would be too close
+                 * to the end. As such, the following space available
+                 * check will always pass
+                 */
+
                 space_available = llq_len - widx;
             } else {
+                /* In this case the writer could actually be catching
+                 * up to the reader and the following space avaialable
+                 * check could fail which either results in a drop or
+                 * in blocking-mode the loop is restarted via the goto
+                 */
+
                 space_available = cur_ridx - widx;
             }
         }
@@ -83,6 +139,7 @@ struct ll_queue {
         }
     }
 
+
     void send(ssize_t length) {
         struct llq_msg *m = (struct llq_msg *)&rbuf[widx];
 
@@ -92,6 +149,9 @@ struct ll_queue {
 
         uint64_t new_widx = widx + sizeof(struct llq_msg) + length;
 
+        /* There must be space free at the end of the ringbuffer for a
+        * struct + the max message size or else we wrap to the start.
+        */
         if (new_widx + sizeof(struct llq_msg) + LLQ_MAX_MSG_SIZE >= llq_len) {
             /* wrap ringbuffer */
             new_widx = 0;
@@ -128,11 +188,13 @@ struct ll_queue {
         }
     }
 
+
     void complete_read() {
         struct llq_msg *m = (struct llq_msg *)&rbuf[ridx];
 
         uint64_t new_ridx = ridx + sizeof(struct llq_msg) + m->len;
 
+        /* Same struct + msg wrapping logic as used by the sender */
         if (new_ridx + sizeof(struct llq_msg) + LLQ_MAX_MSG_SIZE >= llq_len) {
             /* wrap ringbuffer */
             new_ridx = 0;
