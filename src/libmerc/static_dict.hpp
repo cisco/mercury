2cb3fb381e9ecd9285c6cd377734f921c6e49abd
David McGrew <mcgrew@cisco.com>
2024-07-04 08:21:05 -0400

    removing extraneous code

--- src/libmerc/static_dict.hpp
+++ src/libmerc/static_dict.hpp
@@ -7,43 +7,6 @@
 
 #include <iterator>   // for std::distance()
 
-namespace detail
-{
-    template<class T, std::size_t N, std::size_t... I>
-    constexpr std::array<std::remove_cv_t<T>, N>
-        to_array_impl(T (&a)[N], std::index_sequence<I...>)
-    {
-        return {{a[I]...}};
-    }
-}
-
-template<class T, std::size_t N>
-constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&a)[N])
-{
-    return detail::to_array_impl(a, std::make_index_sequence<N>{});
-}
-
-namespace detail
-{
-    template<class T, std::size_t N, std::size_t... I>
-    constexpr std::array<std::remove_cv_t<T>, N>
-        to_array_impl(T (&&a)[N], std::index_sequence<I...>)
-    {
-        return {{std::move(a[I])...}};
-    }
-}
-
-template<class T, std::size_t N>
-constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&&a)[N])
-{
-    return detail::to_array_impl(std::move(a), std::make_index_sequence<N>{});
-}
-
-
-//
-//
-//
-
 static constexpr bool streq(const char *l, const char *r) {
     while (*l and *r) {
         if (*l != *r) {
