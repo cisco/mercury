127b8f13fe04c65e86620c1915d0d0d03247d997
Andrew Chi <andrchi@cisco.com>
2024-11-04 17:32:06 -0500

    loopback: handle both big-endian and little-endian

--- src/libmerc/loopback.hpp
+++ src/libmerc/loopback.hpp
@@ -9,12 +9,14 @@
 // The protocol type field is in the host byte order of the machine on
 // which the capture was done. The values for that field are:
 //
-// 2 - payload is an IPv4 packet;
+// 2  - payload is an IPv4 packet;
 // 24 - payload is an IPv6 packet;
 // 28 - payload is an IPv6 packet;
 // 30 - payload is an IPv6 packet;
-// 7 - payload is an OSI packet;
+// 7  - payload is an OSI packet;
 // 23 - payload is an IPX packet.
+//
+// Note that we must handle both big-endian and little-endian values.
 
 class loopback_header {
     encoded<uint32_t> protocol_type;
@@ -25,15 +27,21 @@ public:
 
     uint16_t get_protocol_type() const {
         switch(protocol_type.value()) {
-        case 2:
-            return ETH_TYPE_IP;
-        case 24:
-        case 28:
-        case 30:
-            return ETH_TYPE_IP;
-        case 7:
+        case 0x00000002: // 2
+        case 0x02000000:
+            return ETH_TYPE_IP; // IPv4
+        case 0x00000018: // 24
+        case 0x18000000:
+        case 0x0000001c: // 28
+        case 0x1c000000:
+        case 0x0000001e: // 30
+        case 0x1e000000:
+            return ETH_TYPE_IP; // IPv6
+        case 0x00000007: // 7
+        case 0x07000000:
             // actually OSI packets
-        case 23:
+        case 0x00000017: // 23
+        case 0x17000000:
             // actually IPX packets
         default:
             ;
