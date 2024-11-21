7bda454a3c7d9178a9244ee34e1354cb315672d9
David McGrew <mcgrew@cisco.com>
2024-11-21 14:49:08 -0500

    experimental bind_request parsing

--- src/libmerc/ldap.hpp
+++ src/libmerc/ldap.hpp
@@ -35,26 +35,34 @@ namespace ldap {
 //        credentials             OCTET STRING OPTIONAL }
 
     class bind_request {
-        tlv outer_sequence;
-        tlv message_id;
-        tlv sequence;
+        // tlv outer_sequence;
+        // tlv message_id;
+        // tlv sequence;
         tlv version;
+        tlv ldapdn;
+        tlv auth;
 
     public:
 
         bind_request(datum &d) {
-            outer_sequence.parse(&d, tlv::SEQUENCE);
-            message_id.parse(&outer_sequence.value, tlv::INTEGER);
-            sequence.parse(&outer_sequence.value);
+            // outer_sequence.parse(&d, tlv::SEQUENCE);
+            // message_id.parse(&outer_sequence.value, tlv::INTEGER);
+            // sequence.parse(&d);
             //            sequence.parse(&outer_sequence.value, tlv::SEQUENCE);
             //   version.parse(&sequence.value, tlv::INTEGER);
-            version.parse(&sequence.value, tlv::INTEGER);
+            version.parse(&d, tlv::INTEGER);
+            ldapdn.parse(&d);
+            auth.parse(&d);
 
         }
 
         void write_json(json_object &o) const {
-            o.print_key_hex("message_id", message_id.value);
-            o.print_key_hex("version", version.value);
+            json_object_asn1 bind_req_json{o, "bind_request"};
+            // o.print_key_hex("message_id", message_id.value);
+            bind_req_json.print_key_hex("version", version.value);
+            bind_req_json.print_key_hex("ldapdn", ldapdn.value);
+            bind_req_json.print_key_hex("auth", auth.value);
+            bind_req_json.close();
         }
 
     };
@@ -94,22 +102,53 @@ namespace ldap {
         tlv outer_sequence; // SEQUENCE
         tlv message_id;     // INTEGER
         tlv protocol_op;    // ???
+        bool valid{false};
 
     public:
 
         message(datum &d) {
-            // fprintf(stderr, "found ldap port\n");
-            outer_sequence.parse(&d, tlv::SEQUENCE);
-            message_id.parse(&outer_sequence.value, tlv::INTEGER);
+            if (d.length() == 0) {
+                return;  // don't process zero-length inputs
+            }
+            // fprintf(stderr, "ldap::message::%s\n", __func__);
+            // d.fprint_hex(stderr); fputc('\n', stderr);
+            outer_sequence.parse(&d, tlv::SEQUENCE, "outer_sequence");
+            message_id.parse(&outer_sequence.value, tlv::INTEGER, "message_id");
+            valid = message_id.value.is_not_empty();
+            protocol_op.parse(&outer_sequence.value, 0x00, "protocol_op");
+
+            // valid = d.is_not_null();
+            // fprintf(stderr, "datum after parsing:\t");
+            // d.fprint_hex(stderr); fputc('\n', stderr);
         }
 
         bool is_not_empty() const {
-            return message_id.is_valid();
+            return valid;
         }
 
         void write_json(struct json_object &o, bool output_metadata) {
+            if (!valid) {
+                return;   // nothing to report on
+            }
             (void)output_metadata;
-            json_object ldap_json{o, "ldap"};
+            json_object_asn1 ldap_json{o, "ldap"};
+            ldap_json.print_key_hex("message_id", message_id.value);
+            // ldap_json.print_key_hex("protocol_op", protocol_op.value);
+
+            constexpr uint8_t bind_req_tag = 0x60;  // TODO: make this programmatic
+            switch(protocol_op.tag) {
+            case bind_req_tag:
+                {
+                    // fprintf(stderr, "got bind_req_tag\n");
+                    bind_request bind_req{protocol_op.value};
+                    bind_req.write_json(ldap_json);
+                }
+                break;
+            default:
+                // fprintf(stderr, "got tag %02x (not %02x or %02x)\n", protocol_op.tag, bind_req_tag, tlv::explicit_tag_constructed(0));
+                ;
+            }
+
             ldap_json.close();
         }
 
