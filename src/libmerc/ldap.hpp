1000105277f9d3b0f4f6edfb9c35f86c60472159
David McGrew <mcgrew@cisco.com>
2024-12-07 11:53:44 -0500

    adding bind_request.auth details and bind_response

--- src/libmerc/ldap.hpp
+++ src/libmerc/ldap.hpp
@@ -17,22 +17,231 @@
 
 namespace ldap {
 
-// LDAP BindRequest, following RFC 4511
-//
-//   BindRequest ::= [APPLICATION 0] SEQUENCE {
-//        version                 INTEGER (1 ..  127),
-//        name                    LDAPDN,
-//        authentication          AuthenticationChoice }
-//
-//   AuthenticationChoice ::= CHOICE {
-//        simple                  [0] OCTET STRING,
-//                                -- 1 and 2 reserved
-//        sasl                    [3] SaslCredentials,
-//        ...  }
-//
-//   SaslCredentials ::= SEQUENCE {
-//        mechanism               LDAPString,
-//        credentials             OCTET STRING OPTIONAL }
+    //   LDAPResult ::= SEQUENCE {
+    //         resultCode         ENUMERATED {
+    //              success                      (0),
+    //              operationsError              (1),
+    //              protocolError                (2),
+    //              timeLimitExceeded            (3),
+    //              sizeLimitExceeded            (4),
+    //              compareFalse                 (5),
+    //              compareTrue                  (6),
+    //              authMethodNotSupported       (7),
+    //              strongerAuthRequired         (8),
+    //                   -- 9 reserved --
+    //              referral                     (10),
+    //              adminLimitExceeded           (11),
+    //              unavailableCriticalExtension (12),
+    //              confidentialityRequired      (13),
+    //              saslBindInProgress           (14),
+    //              noSuchAttribute              (16),
+    //              undefinedAttributeType       (17),
+    //              inappropriateMatching        (18),
+    //              constraintViolation          (19),
+    //              attributeOrValueExists       (20),
+    //              invalidAttributeSyntax       (21),
+    //                   -- 22-31 unused --
+    //              noSuchObject                 (32),
+    //              aliasProblem                 (33),
+    //              invalidDNSyntax              (34),
+    //                   -- 35 reserved for undefined isLeaf --
+    //              aliasDereferencingProblem    (36),
+    //                   -- 37-47 unused --
+    //              inappropriateAuthentication  (48),
+    //              invalidCredentials           (49),
+    //              insufficientAccessRights     (50),
+    //              busy                         (51),
+    //              unavailable                  (52),
+    //              unwillingToPerform           (53),
+    //              loopDetect                   (54),
+    //                   -- 55-63 unused --
+    //              namingViolation              (64),
+    //              objectClassViolation         (65),
+    //              notAllowedOnNonLeaf          (66),
+    //              notAllowedOnRDN              (67),
+    //              entryAlreadyExists           (68),
+    //              objectClassModsProhibited    (69),
+    //                   -- 70 reserved for CLDAP --
+    //              affectsMultipleDSAs          (71),
+    //                   -- 72-79 unused --
+    //              other                        (80),
+    //              ...  },
+    //         matchedDN          LDAPDN,
+    //         diagnosticMessage  LDAPString,
+    //         referral           [3] Referral OPTIONAL }
+
+    enum result_codes {
+        success                      = 0,
+        operationsError              = 1,
+        protocolError                = 2,
+        timeLimitExceeded            = 3,
+        sizeLimitExceeded            = 4,
+        compareFalse                 = 5,
+        compareTrue                  = 6,
+        authMethodNotSupported       = 7,
+        strongerAuthRequired         = 8,
+        referral                     = 10,
+        adminLimitExceeded           = 11,
+        unavailableCriticalExtension = 12,
+        confidentialityRequired      = 13,
+        saslBindInProgress           = 14,
+        noSuchAttribute              = 16,
+        undefinedAttributeType       = 17,
+        inappropriateMatching        = 18,
+        constraintViolation          = 19,
+        attributeOrValueExists       = 20,
+        invalidAttributeSyntax       = 21,
+        noSuchObject                 = 32,
+        aliasProblem                 = 33,
+        invalidDNSyntax              = 34,
+        aliasDereferencingProblem    = 36,
+        inappropriateAuthentication  = 48,
+        invalidCredentials           = 49,
+        insufficientAccessRights     = 50,
+        busy                         = 51,
+        unavailable                  = 52,
+        unwillingToPerform           = 53,
+        loopDetect                   = 54,
+        namingViolation              = 64,
+        objectClassViolation         = 65,
+        notAllowedOnNonLeaf          = 66,
+        notAllowedOnRDN              = 67,
+        entryAlreadyExists           = 68,
+        objectClassModsProhibited    = 69,
+        affectsMultipleDSAs          = 71,
+        other                        = 80,
+    };
+
+    static const char *get_name(const tlv &resp_code) {
+        uint8_t code{0xff};
+        if (resp_code.is_valid() and resp_code.length == 1) {
+            code = *resp_code.value.data;
+        }
+        switch(code) {
+        case success:                      return "success";
+        case operationsError:              return "operationsError";
+        case protocolError:                return "protocolError";
+        case timeLimitExceeded:            return "timeLimitExceeded";
+        case sizeLimitExceeded:            return "sizeLimitExceeded";
+        case compareFalse:                 return "compareFalse";
+        case compareTrue:                  return "compareTrue";
+        case authMethodNotSupported:       return "authMethodNotSupported";
+        case strongerAuthRequired:         return "strongerAuthRequired";
+        case referral:                     return "referral";
+        case adminLimitExceeded:           return "adminLimitExceeded";
+        case unavailableCriticalExtension: return "unavailableCriticalExtension";
+        case confidentialityRequired:      return "confidentialityRequired";
+        case saslBindInProgress:           return "saslBindInProgress";
+        case noSuchAttribute:              return "noSuchAttribute";
+        case undefinedAttributeType:       return "undefinedAttributeType";
+        case inappropriateMatching:        return "inappropriateMatching";
+        case constraintViolation:          return "constraintViolation";
+        case attributeOrValueExists:       return "attributeOrValueExists";
+        case invalidAttributeSyntax:       return "invalidAttributeSyntax";
+        case noSuchObject:                 return "noSuchObject";
+        case aliasProblem:                 return "aliasProblem";
+        case invalidDNSyntax:              return "invalidDNSyntax";
+        case aliasDereferencingProblem:    return "aliasDereferencingProblem";
+        case inappropriateAuthentication:  return "inappropriateAuthentication";
+        case invalidCredentials:           return "invalidCredentials";
+        case insufficientAccessRights:     return "insufficientAccessRights";
+        case busy:                         return "busy";
+        case unavailable:                  return "unavailable";
+        case unwillingToPerform:           return "unwillingToPerform";
+        case loopDetect:                   return "loopDetect";
+        case namingViolation:              return "namingViolation";
+        case objectClassViolation:         return "objectClassViolation";
+        case notAllowedOnNonLeaf:          return "notAllowedOnNonLeaf";
+        case notAllowedOnRDN:              return "notAllowedOnRDN";
+        case entryAlreadyExists:           return "entryAlreadyExists";
+        case objectClassModsProhibited:    return "objectClassModsProhibited";
+        case affectsMultipleDSAs:          return "affectsMultipleDSAs";
+        case other:                        return "other";
+        default:
+            ;
+        }
+        return nullptr;
+    }
+
+    // LDAP BindResponse, following RFC 4511
+    //
+    // BindResponse ::= [APPLICATION 1] SEQUENCE {
+    //      COMPONENTS OF LDAPResult,
+    //      serverSaslCreds    [7] OCTET STRING OPTIONAL }
+    //
+    class bind_response {
+        tlv result_code;
+        tlv matched_dn;
+        tlv diagnostic_msg;
+        // tlv referral;
+        // TODO: server_sasl_creds
+        bool valid{false};
+
+    public:
+
+        void parse(datum &d) {
+            result_code.parse(&d);
+            matched_dn.parse(&d);
+            diagnostic_msg.parse(&d);
+            valid = d.is_not_null();
+        }
+
+        void write_json(json_object_asn1 &o) const {
+            if (!valid) { return; }
+            json_object_asn1 bind_resp_json{o, "bind_response"};
+            const char *result_name = get_name(result_code);
+            bind_resp_json.print_key_string("result_code", result_name);
+            // result_code.print_as_json(bind_resp_json, "result_code");
+            matched_dn.print_as_json(bind_resp_json, "matched_dn");
+            diagnostic_msg.print_as_json(bind_resp_json, "diagnostic_message");
+            bind_resp_json.close();
+        }
+
+    };
+
+    // LDAP BindRequest, following RFC 4511
+    //
+    //   BindRequest ::= [APPLICATION 0] SEQUENCE {
+    //        version                 INTEGER (1 ..  127),
+    //        name                    LDAPDN,
+    //        authentication          AuthenticationChoice }
+    //
+    //   AuthenticationChoice ::= CHOICE {
+    //        simple                  [0] OCTET STRING,
+    //                                -- 1 and 2 reserved
+    //        sasl                    [3] SaslCredentials,
+    //        ...  }
+    //
+    //   SaslCredentials ::= SEQUENCE {
+    //        mechanism               LDAPString,
+    //        credentials             OCTET STRING OPTIONAL }
+
+    class sasl_credentials {
+        tlv mechanism;
+        tlv credentials;
+        bool valid;
+
+    public:
+
+        // sasl_credentials(datum &) { }
+
+        void parse(datum &d) {
+            mechanism.parse(&d, 0x00, "sasl_credentials.mechanism");
+            credentials.parse(&d, tlv::OCTET_STRING, "sasl_credentials.credentials");
+            valid = d.is_not_null();
+        }
+
+        bool is_not_empty() const { return valid; }
+
+        void write_json(json_object_asn1 &o) const {
+            if (!valid) { return; }
+            json_object_asn1 sasl_json{o, "sasl"};
+            mechanism.print_as_json_escaped_string(sasl_json, "mechanism");
+            credentials.print_as_json_hex(sasl_json, "credentials");  // TODO: replace with asn1 parsing
+            sasl_json.close();
+        }
+
+    };
 
     class bind_request {
         // tlv outer_sequence;
@@ -50,9 +259,9 @@ namespace ldap {
             // sequence.parse(&d);
             //            sequence.parse(&outer_sequence.value, tlv::SEQUENCE);
             //   version.parse(&sequence.value, tlv::INTEGER);
-            version.parse(&d, tlv::INTEGER);
-            ldapdn.parse(&d);
-            auth.parse(&d);
+            version.parse(&d, tlv::INTEGER, "version");
+            ldapdn.parse(&d, 0x00, "ldapdn");
+            auth.parse(&d, 0x00, "auth");
 
         }
 
@@ -60,8 +269,21 @@ namespace ldap {
             json_object_asn1 bind_req_json{o, "bind_request"};
             // o.print_key_hex("message_id", message_id.value);
             bind_req_json.print_key_hex("version", version.value);
-            bind_req_json.print_key_hex("ldapdn", ldapdn.value);
-            bind_req_json.print_key_hex("auth", auth.value);
+            bind_req_json.print_key_uint_hex("ldapdn_tag", ldapdn.tag);
+            bind_req_json.print_key_uint_hex("auth_tag", auth.tag);
+            ldapdn.print_as_json_escaped_string(bind_req_json, "ldapdn");
+            json_object_asn1 auth_json{bind_req_json, "auth"};
+            if (auth.tag == 0x80) {
+                auth.print_as_json_escaped_string(auth_json, "simple");
+            } else if (auth.tag == 0xa3) {
+                datum tmp = auth.value;
+                sasl_credentials sasl_cred;
+                sasl_cred.parse(tmp);
+                sasl_cred.write_json(auth_json);
+            } else {
+                auth_json.print_key_hex("unknown", auth.value);
+            }
+            auth_json.close();
             bind_req_json.close();
         }
 
@@ -136,12 +358,21 @@ namespace ldap {
             // ldap_json.print_key_hex("protocol_op", protocol_op.value);
 
             constexpr uint8_t bind_req_tag = 0x60;  // TODO: make this programmatic
+            constexpr uint8_t bind_resp_tag = 0x61; // TODO: make this programmatic
             switch(protocol_op.tag) {
             case bind_req_tag:
                 {
                     // fprintf(stderr, "got bind_req_tag\n");
                     bind_request bind_req{protocol_op.value};
                     bind_req.write_json(ldap_json);
+                }
+                break;
+            case bind_resp_tag:
+                {
+                    // fprintf(stderr, "got bind_req_tag\n");
+                    bind_response bind_resp;
+                    bind_resp.parse(protocol_op.value);
+                    bind_resp.write_json(ldap_json);
                 }
                 break;
             default:
