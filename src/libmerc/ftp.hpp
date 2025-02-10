a72e51b6aa3fd72dff308a7370cc2cff239a7f9b
Chinmay Parekh <chiparek@cisco.com>
2025-02-10 20:48:36 +0530

    formatted the code

--- src/libmerc/ftp.hpp
+++ src/libmerc/ftp.hpp
@@ -6,41 +6,52 @@
 #include "lex.h"
 #include "match.h"
 
-class up_to_byte : public datum {
+class up_to_byte : public datum
+{
 public:
-    up_to_byte(datum &d, uint8_t byte) {
+    up_to_byte(datum &d, uint8_t byte)
+    {
         const uint8_t *location = (const uint8_t *)memchr(d.data, byte, d.length());
-        if (location == nullptr) {
+        if (location == nullptr)
+        {
             data = nullptr;
             data_end = nullptr;
             d.set_null();
-        } else if (data_end > data) {
+        }
+        else if (data_end > data)
+        {
             data_end = location - 1;
         }
         data = d.data;
     }
 };
 
-class up_to_crlf : public datum {
+class up_to_crlf : public datum
+{
 public:
-    up_to_crlf(datum &d) {
+    up_to_crlf(datum &d)
+    {
         this->parse_up_to_delim(d, '\r');
         optional<literal_byte<'\n'>> lf{d};
     }
 };
 
-namespace ftp {
+namespace ftp
+{
 
     // FTP command: Section 5.3.1 - Uppercase ASCII, 3 or 4 characters
-    class ftp_command : public one_or_more<ftp_command> {
+    class ftp_command : public one_or_more<ftp_command>
+    {
     public:
-        inline static bool in_class(uint8_t x) {
+        inline static bool in_class(uint8_t x)
+        {
             return (x >= 'A' && x <= 'Z');
         }
     };
 
     // USER <SP> <username> <CRLF>
-    class request : public base_protocol {
+    class request : public base_protocol
+    {
         ftp_command command;
         literal_byte<' '> sp;
         up_to_crlf argument;
@@ -48,7 +59,8 @@ namespace ftp {
     public:
         request(datum &d) : command{d}, sp{d}, argument{d} {}
 
-        void write_json(struct json_object &record, bool) {
+        void write_json(struct json_object &record, bool)
+        {
             struct json_object ftp_object{record, "ftp"};
             struct json_object ftp_request{ftp_object, "request"};
             ftp_request.print_key_json_string("command", command);
@@ -78,11 +90,11 @@ namespace ftp {
         //     { 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00 },
         //     { 'S', 'T', 'O', 'R', ' ', 0x00, 0x00, 0x00 }
         // };
-        
     };
 
     // From RFC 959 Section 4.2: Reply format
-    class response : public base_protocol {
+    class response : public base_protocol
+    {
         digits status_code;
         literal_byte<' '> sp;
         up_to_crlf reply_text;
@@ -90,14 +102,15 @@ namespace ftp {
     public:
         response(datum &d) : status_code{d}, sp{d}, reply_text{d} {}
 
-        void write_json(struct json_object &record, bool) {
-        struct json_object ftp_object{record, "ftp"};
-        struct json_object ftp_response{ftp_object, "response"};
-        ftp_response.print_key_json_string("status_code", status_code);
-        ftp_response.print_key_json_string("reply_text", reply_text);
-        ftp_response.close();
-        ftp_object.close();
-    }
+        void write_json(struct json_object &record, bool)
+        {
+            struct json_object ftp_object{record, "ftp"};
+            struct json_object ftp_response{ftp_object, "response"};
+            ftp_response.print_key_json_string("status_code", status_code);
+            ftp_response.print_key_json_string("reply_text", reply_text);
+            ftp_response.close();
+            ftp_object.close();
+        }
 
         bool is_not_empty() const { return status_code.is_not_empty(); }
 
@@ -113,12 +126,12 @@ namespace ftp {
         //     { 0xf0, 0xf0, 0xf0, 0xf0 },  // Masks for detecting digit characters (0-9) followed by space/hyphen
         //     { '0',  '0',  '0',  ' ' }    // Base pattern for a generic FTP status code "000 " or "000-"
         // };
-
     };
 
-    #ifndef NDEBUG
-    static bool unit_test() {
-        
+#ifndef NDEBUG
+    static bool unit_test()
+    {
+
         // True positive test: valid FTP request for USER command
         // uint8_t user_command_packet[] = {
         //     'U', 'S', 'E', 'R', ' ', 'f', 't', 'p', 'u', 's', 'e', 'r', '\r', '\n'
@@ -142,26 +155,27 @@ namespace ftp {
         uint8_t garbage_packet[20] = {
             0xff, 0xff, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
             0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0xaa, 0xbb,
-            0xcc, 0xdd, 0xee, 0xff
-        };
+            0xcc, 0xdd, 0xee, 0xff};
         datum garbage{garbage_packet, garbage_packet + sizeof(garbage_packet)};
         ftp::request invalid_request{garbage};
-        if (invalid_request.is_not_empty()) {
+        if (invalid_request.is_not_empty())
+        {
             return false;
         }
 
         // False positive test: invalid garbage response
         ftp::response invalid_response{garbage};
-        if (invalid_response.is_not_empty()) {
+        if (invalid_response.is_not_empty())
+        {
             return false;
         }
 
         return true;
     }
 
-    static inline bool unit_test_passed= ftp::unit_test();
+    static inline bool unit_test_passed = ftp::unit_test();
 
-#endif 
+#endif
 };
 
 #endif // FTP_HPP
