94e13cbac8d0a5c270c9c939df8e2455ac1992b9
Chinmay Parekh <chiparek@cisco.com>
2025-02-12 10:56:16 +0530

    Adding support for multi-line response parsing

--- src/libmerc/ftp.hpp
+++ src/libmerc/ftp.hpp
@@ -6,26 +6,6 @@
 #include "lex.h"
 #include "match.h"
 
-// class up_to_byte : public datum
-// {
-// public:
-//     up_to_byte(datum &d, uint8_t byte)
-//     {
-//         const uint8_t *location = (const uint8_t *)memchr(d.data, byte, d.length());
-//         if (location == nullptr)
-//         {
-//             data = nullptr;
-//             data_end = nullptr;
-//             d.set_null();
-//         }
-//         else if (data_end > data)
-//         {
-//             data_end = location;
-//         }
-//         data = d.data;
-//     }
-// };
-
 class up_to_crlf : public datum
 {
 public:
@@ -93,14 +73,26 @@ namespace ftp
     };
 
     // From RFC 959 Section 4.2: Reply format
+    // single-line response: <status_code> <reply-text>
+
+    // multi-line response:
+    //<status_code>-<reply-text>\n
+    //...
+    //<status_code> <reply-text>\n
+
     class response : public base_protocol
     {
         digits status_code;
-        literal_byte<' '> sp;
-        up_to_crlf reply_text;
+        datum reply_text;
 
     public:
-        response(datum &d) : status_code{d}, sp{d}, reply_text{d} {}
+        response(datum &d) : status_code{d}
+        {
+            d.skip(1);
+            reply_text = d;
+        }
+
+        bool is_not_empty() const { return status_code.is_not_empty(); }
 
         void write_json(struct json_object &record, bool)
         {
@@ -111,46 +103,11 @@ namespace ftp
             ftp_response.close();
             ftp_object.close();
         }
-
-        bool is_not_empty() const { return status_code.is_not_empty(); }
-
-        // static constexpr mask_and_value<4> status_code_matcher
-        // {
-        //     { 0xff, 0xff, 0xff, 0xff },
-        //     { '2',  '2',  '0',  ' ' }  // Detect "220 " pattern
-        // };
-
-        // static constexpr mask_and_value<4> status_code_matcher
-        // {
-        //     // detection of ASCII digits (0x30 to 0x39)
-        //     { 0xf0, 0xf0, 0xf0, 0xf0 },  // Masks for detecting digit characters (0-9) followed by space/hyphen
-        //     { '0',  '0',  '0',  ' ' }    // Base pattern for a generic FTP status code "000 " or "000-"
-        // };
     };
 
 #ifndef NDEBUG
     static bool unit_test()
     {
-
-        // True positive test: valid FTP request for USER command
-        // uint8_t user_command_packet[] = {
-        //     'U', 'S', 'E', 'R', ' ', 'f', 't', 'p', 'u', 's', 'e', 'r', '\r', '\n'
-        // };
-        // datum user_command{user_command_packet, user_command_packet + sizeof(user_command_packet)};
-        // ftp::request valid_request{user_command};
-        // if (!valid_request.is_not_empty()) {
-        //     return false;
-        // }
-
-        // uint8_t valid_response_packet[] = {
-        //     '2', '2', '0', ' ', 'S', 'e', 'r', 'v', 'e', 'r', ' ', 'R', 'e', 'a', 'd', 'y', '\r', '\n'
-        // };
-        // datum response_datum1{valid_response_packet, valid_response_packet + sizeof(valid_response_packet)};
-        // ftp::response valid_response1{response_datum1};
-        // if (!valid_response1.is_not_empty()) {
-        //     return false;
-        // }
-
         // False positive test: invalid garbage packet for request
         uint8_t garbage_packet[20] = {
             0xff, 0xff, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
