d7bf5e65489473898f173fd17a365a9360e3d811
David McGrew <mcgrew@cisco.com>
2025-03-06 14:50:31 -0500

    removing dupliate class definition

--- src/libmerc/ip_address.hpp
+++ src/libmerc/ip_address.hpp
@@ -527,13 +527,6 @@ public:
 
 };
 
-class hex_digits : public one_or_more<hex_digits> {
-public:
-    inline static bool in_class(uint8_t x) {
-        return (x >= '0' && x <= '9') || (x >= 'a' && x <= 'f') || (x >= 'A' && x <= 'F');
-    }
-};
-
 template <typename T>
 T hex_str_to_uint(const hex_digits &d) {
     T tmp = 0;
