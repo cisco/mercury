7ecf14111cb66f0ca840fdedb68ae1d4327fa04e
David McGrew <mcgrew@cisco.com>
2025-07-17 08:21:23 -0400

    adding --verbose option and fixing format specifier

--- src/cbor.cpp
+++ src/cbor.cpp
@@ -29,6 +29,7 @@ int main(int argc, char *argv[]) {
             { argument::none,     "--encode-fingerprint", "encode fingerprint string as CBOR" },
             { argument::required, "--input-file",         "read data from file <filename>" },
             { argument::none,     "--verbose-tests",      "run unit tests in verbose mode" },
+            { argument::none,     "--verbose",            "provide verbose output" },
             { argument::none,     "--help",               "print out help message" },
         });
 
@@ -42,11 +43,14 @@ int main(int argc, char *argv[]) {
     bool decode_fdc     = opt.is_set("--decode-fdc");
     bool encode_fp      = opt.is_set("--encode-fingerprint");
     bool verbose_tests  = opt.is_set("--verbose-tests");
+    bool verbose        = opt.is_set("--verbose");
     bool help_needed    = opt.is_set("--help");
     if (help_needed) {
         opt.usage(stdout, argv[0], summary);
         return 0;
     }
+
+    FILE *verbose_output = verbose ? stderr : nullptr;
 
     if (verbose_tests) {
         bool sd_result = static_dictionary<0>::unit_test(stdout);
@@ -105,7 +109,7 @@ int main(int argc, char *argv[]) {
                 fprintf(stdout, "\"dst_ip_str\": \"%s\",", dst_ip_str);
                 fprintf(stdout, "\"dst_port\": %u,", dst_port);
                 fprintf(stdout, "\"user-agent\": \"%s\",", ua_str);
-                fprintf(stdout, "\"truncation\": %u", truncation);
+                fprintf(stdout, "\"truncation\": %lu", truncation);
                 fprintf(stdout, "}}\n");
             } else {
                 fprintf(stderr, "error: could not decode FDC\n");
@@ -123,7 +127,7 @@ int main(int argc, char *argv[]) {
 
             // verify that we can represent this fingerprint in CBOR
             //
-            if (!cbor_fingerprint::test_fingerprint(line.c_str())){
+            if (!cbor_fingerprint::test_fingerprint(line.c_str()), verbose_output){
                 fprintf(stderr, "error: could not encode/decode fingerprint %s\n", line.c_str());
                 return EXIT_FAILURE;
             }
