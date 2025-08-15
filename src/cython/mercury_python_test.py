c2d5beddae31b68cc4b579fd4316806cc0938e51
blaander@cisco.com <blaander@cisco.com>
2025-08-15 16:48:01 -0400

    enrichment: cython-based test for mercury domain normalization classification

--- src/cython/mercury_python_test.py
+++ src/cython/mercury_python_test.py
@@ -123,5 +123,17 @@ class TestMercuryPython(unittest.TestCase):
                          f"FakeTLS should be the only attribute in the unlabeled case")
 
 
+    def test_normalized_domain_result(self):
+        str_repr    = 'http/(0123)(4567)((89abcdef))'
+        dst_ip      = '1.1.1.1'
+        server_name = '1.1.1.1:443'
+        dst_port    = 443
+
+        cls_result = self.libmerc.perform_analysis(str_repr, server_name, dst_ip, dst_port)
+        print(cls_result)
+        self.assertEqual(cls_result['analysis']['process'], f"good",
+                         f"The classifier is using 1.1:443 as the domain feature")
+
+
 if __name__ == '__main__':
     unittest.main()
