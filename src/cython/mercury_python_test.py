e8c0e9f47bed3555e6fd79a22eeba83b08a3c23d
blaander@cisco.com <blaander@cisco.com>
2025-08-20 15:33:50 -0400

    cython: cbor decoding test

--- src/cython/mercury_python_test.py
+++ src/cython/mercury_python_test.py
@@ -123,5 +123,19 @@ class TestMercuryPython(unittest.TestCase):
                          f"FakeTLS should be the only attribute in the unlabeled case")
 
 
+    def test_cbor_decoding(self):
+        fdc = ('vwGfvwG/AZ9CAwNYJMAswCvAMMAvwCTAI8AowCfACsAJwBTAEwCdAJwAPQA8ADUAL9j7n0IAAEkABQAFAQAAAABMAAoACAAG'
+               'AB0AFwAYRgALAAIBAFgeAA0AGgAYCAQIBQgGBAEFAQIBBAMFAwIDAgIGAQYDUgAQAA4ADAJoMghodHRwLzEuMUIAF0IAI0L/'
+               'Af////94H3NldHRpbmdzLXdpbi5kYXRhLm1pY3Jvc29mdC5jb21sNTIuMTY3LjE3Ljk3GQG7YP//')
+
+        decoded_data = mercury.decode_mercury_fdc(fdc)
+        self.assertEqual(decoded_data['fdc']['sni'], f"settings-win.data.microsoft.com",
+                         f"Domain feature decoded improperly")
+        self.assertEqual(decoded_data['fdc']['dst_ip_str'], f"52.167.17.97",
+                         f"Destination IP feature decoded improperly")
+        self.assertEqual(decoded_data['fdc']['dst_port'], 443,
+                         f"Destination port feature decoded improperly")
+
+
 if __name__ == '__main__':
     unittest.main()
