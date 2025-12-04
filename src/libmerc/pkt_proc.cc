1dffae1c6fcde5f0cb5a9ca55bc4f79e0f680dc2
David McGrew <mcgrew@cisco.com>
2025-12-04 13:10:08 -0500

    moving events into their own file

--- src/libmerc/pkt_proc.cc
+++ src/libmerc/pkt_proc.cc
@@ -19,6 +19,7 @@
 #include "utils.h"
 #include "loopback.hpp"
 #include "linux_sll.hpp"
+#include "event.hpp"
 
 // include files needed by stateful_pkt_proc; they provide the
 // interface to mercury's packet parsing and handling routines
@@ -148,61 +149,6 @@ struct do_crypto_assessment {
 
     bool operator()(std::monostate &) { return false; }
 
-};
-
-template <typename T_M>
-class event_string
-{
-    const struct key &k;
-    const struct analysis_context &analysis;
-    std::string dest_context;
-    event_msg event;
-    T_M &message_pkt;
-
-public:
-    event_string(const struct key &k, const struct analysis_context &analysis, T_M &proto) :
-        k{k}, analysis{analysis}, message_pkt{proto} {  }
-
-    event_msg construct_event_string_proto( [[maybe_unused]] tofsee_initial_message &msg) {
-        // For tofsee initial pkt, src ip, src port and bot ip are important
-        // replace dst ip and port with src ip and port
-        // add bot ip as user agent string
-        //
-        char src_ip_str[MAX_ADDR_STR_LEN];
-        k.sprintf_dst_addr(src_ip_str);
-        char dst_ip_str[MAX_ADDR_STR_LEN];
-        k.sprint_src_addr(dst_ip_str);
-        char dst_port_str[MAX_PORT_STR_LEN];
-        k.sprint_src_port(dst_port_str);
-
-        dest_context.append("(");
-        dest_context.append(analysis.destination.sn_str).append(")(");
-        dest_context.append(dst_ip_str).append(")(");
-        dest_context.append(dst_port_str).append(")");
-
-        event = std::make_tuple(src_ip_str, analysis.fp.string(), analysis.destination.ua_str, dest_context);
-        return event;
-    }
-
-    template <typename T>
-    event_msg construct_event_string_proto([[maybe_unused]] T &msg) {
-        char src_ip_str[MAX_ADDR_STR_LEN];
-        k.sprint_src_addr(src_ip_str);
-        char dst_port_str[MAX_PORT_STR_LEN];
-        k.sprint_dst_port(dst_port_str);
-
-        dest_context.append("(");
-        dest_context.append(analysis.destination.sn_str).append(")(");
-        dest_context.append(analysis.destination.dst_ip_str).append(")(");
-        dest_context.append(dst_port_str).append(")");
-
-        event = std::make_tuple(src_ip_str, analysis.fp.string(), utf8_string::get_utf8_string(analysis.destination.ua_str), dest_context);
-        return event;
-    }
-
-    event_msg construct_event_string() {
-        return construct_event_string_proto(message_pkt);
-    }
 };
 
 struct do_observation {
