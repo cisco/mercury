3ef77a375a13984a7c6fbfcf5cdbe3a7e1da29c6
David McGrew <mcgrew@cisco.com>
2026-02-12 21:25:43 +0000

    adding type trait and documentation

--- src/libmerc/x509.h
+++ src/libmerc/x509.h
@@ -1598,10 +1598,14 @@ struct x509_cert {
     /// write this certificate as a json object into \p buf, using the
     /// trusted root list \p trusted_certs and a key group \p
     /// key_group, and invoking \p json_writer on the object (to allow
-    /// the addition of arbitrary json elements)
+    /// the addition of arbitrary json elements).  The template
+    /// parameter \p F is a callable object that is invocable with a
+    /// \ref json_object &.
+    ///
     ///
     template <typename F>
     void print_as_json(struct buffer_stream &buf, const std::list<struct x509_cert> &trusted_certs, struct dictionary *key_group, const F &json_writer) const {
+        static_assert(std::is_invocable_v<F, json_object&>, "F must be callable with (json_object&)");
         struct json_object o{&buf};
         print_as_json(o, trusted_certs, key_group);
         json_writer(o);
