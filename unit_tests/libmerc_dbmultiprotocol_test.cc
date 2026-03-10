c2dab30ee5031d93b13f8c4031dc1dbb30066370
chinmay1104 <chiparek@cisco.com>
2026-03-10 03:05:23 -0700

    Removed duplicate test

--- unit_tests/libmerc_dbmultiprotocol_test.cc
+++ unit_tests/libmerc_dbmultiprotocol_test.cc
@@ -602,20 +602,6 @@ TEST_CASE_METHOD(LibmercTestFixture, "test crypto_assessment quantum_safe compli
     deinitialize();
 }
 
-TEST_CASE_METHOD(LibmercTestFixture, "test crypto_assessment skipped on truncated tls")
-{
-    set_pcap("tls_cnsa2_psk_mode_psk_ke_truncated.pcap");
-
-    libmerc_config config{.packet_filter_cfg = (char *)"all;crypto-assess=default"};
-
-    initialize(config);
-
-    std::string non_pqc_attr = "cnsa_2_0_non_conformant";
-    CHECK_FALSE(check_attr(non_pqc_attr));
-
-    deinitialize();
-}
-
 TEST_CASE_METHOD(LibmercTestFixture, "test nbss")
 {
     libmerc_config config{.packet_filter_cfg = (char *)"nbss"};
