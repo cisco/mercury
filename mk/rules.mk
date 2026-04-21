8d4a92bf030dff3673647dbc75ff2f6dc4071e78
Andrew Chi <andrchi@cisco.com>
2026-04-21 13:55:55 -0400

    mk/rules.mk: add COLOR_RED for future

--- mk/rules.mk
+++ mk/rules.mk
@@ -51,6 +51,7 @@ else
 endif
 
 # --- Color output -----------------------------------------------------
+COLOR_RED    := \033[0;31m
 COLOR_GREEN  := \033[0;32m
 COLOR_YELLOW := \033[0;33m
 COLOR_OFF    := \033[0m
